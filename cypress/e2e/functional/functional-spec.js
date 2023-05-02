ee25b00a38f18d39619dcf265f8c61ece50771b6
Marc Mollins <marc.mollins@hmcts.net>
2023-05-02 11:09:48 +0100

    DMP-72 functional tests
    removed package-lock.json and updated .yarnrc.yml file to remove node packages
    
    updated functional test

--- cypress/e2e/functional/functional-spec.js
+++ cypress/e2e/functional/functional-spec.js
@@ -1,4 +1,4 @@
-describe('Smoke test', () => {
+describe('functional test', () => {
     it('should load portal', () => {
       cy.visit('/');
       cy.get('h1').should('contain', 'Welcome to DARTS');
