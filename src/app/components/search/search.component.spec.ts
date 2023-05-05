9d2bf688a5d1cb452a61aa4862b9adcd3931c3c8
Dave Thompson <davethompson21@gmail.com>
2023-05-05 15:08:55 +0100

    Adding more tests for search component

--- src/app/components/search/search.component.spec.ts
+++ src/app/components/search/search.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
+import { AppInsightsService } from 'src/app/services/app-insights/app-insights.service';
 
 import { SearchComponent } from './search.component';
 
@@ -12,6 +13,7 @@ describe('SearchComponent', () => {
     await TestBed.configureTestingModule({
       imports: [MatIconModule, ReactiveFormsModule],
       declarations: [SearchComponent],
+      providers: [AppInsightsService],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SearchComponent);
@@ -19,7 +21,23 @@ describe('SearchComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create with empty search term', () => {
     expect(component).toBeTruthy();
+    expect(component.searchTerm.value).toEqual('');
+  });
+
+  it('does search and clear the value', () => {
+    component.searchTerm.setValue('CASE1001');
+    component.search();
+    expect(component.searchTerm.value).toEqual('');
+  });
+
+  it('logs app insights event on search', () => {
+    const spy = spyOn(AppInsightsService.prototype, 'logEvent');
+    component.searchTerm.setValue('CASE1001');
+    component.search();
+
+    expect(spy.calls.count()).withContext('spy method was called once').toBe(1);
+    expect(spy.calls.first().args).toEqual(['CASE_SEARCH', { searchTerm: 'CASE1001' }]);
   });
 });
