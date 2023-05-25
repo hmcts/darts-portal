e89f18951b4c648d94485d2f5b32c843a1aceb92
Dave Thompson <david.thompson@hmcts.net>
2023-05-25 11:33:39 +0100

    Adding POC login template for Azure AD to consume (#47)

--- server/routes.ts
+++ server/routes.ts
@@ -4,12 +4,14 @@ import type { Router } from 'express';
 
 import * as apiController from './controllers/api';
 import * as appController from './controllers/app';
+import * as authController from './controllers/auth';
 
 export default (): Router => {
   const router = express.Router();
 
   router.use(apiController.init());
   router.use(appController.init());
+  router.use(authController.init());
 
   return router;
 };
