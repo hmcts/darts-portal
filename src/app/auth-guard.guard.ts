df3a85b966fc1245a10f0c369acf22f2b0737625
Bredan Pires <bredan.pires@hmcts.net>
2023-06-05 16:47:26 +0100

    removed comments

--- src/app/auth-guard.guard.ts
+++ src/app/auth-guard.guard.ts
@@ -3,9 +3,5 @@ import { CanActivateFn, Router } from '@angular/router';
 
 export const authGuardGuard: CanActivateFn = () => {
   const router = inject(Router);
-  // const authService = inject(authService);
-  // if user is logged in and has a token then return true so the component can activate and user can navigate to the page
-  // return true;
-  // otherwise if the token is not set or invalid then navigate to forbidden page
   return router.navigateByUrl('forbidden');
 };
