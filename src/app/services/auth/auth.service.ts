6079931b09fa85a950c0358b5a558e099cc4696c
Dave Thompson <davethompson21@gmail.com>
2023-06-21 15:14:40 +0100

    Adding timestamp to angular session check
    - this is to prevent caching at the Azure front door level
    - using session save callback to ensure session is set correctly
    - fixing `yarn start` so it works locally

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { lastValueFrom } from 'rxjs';
+import moment from 'moment';
 
 const IS_AUTH_PATH = '/auth/is-authenticated';
 const LOGOUT_PATH = '/auth/logout';
@@ -14,7 +15,8 @@ export class AuthService {
 
   async isAuthenticated(): Promise<boolean> {
     try {
-      await lastValueFrom(this.http.get(IS_AUTH_PATH));
+      // add timestamp to the second to be doubly-sure we are preventing caching
+      await lastValueFrom(this.http.get(`${IS_AUTH_PATH}?t=${moment().format('YYYYMMDDHHmmss')}`));
       return true;
     } catch (err) {
       return false;
