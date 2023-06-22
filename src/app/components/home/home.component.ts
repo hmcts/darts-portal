3ab84a8525b49552a4baa6533a45a4a12003391f
Bredan Pires <bredan.pires@hmcts.net>
2023-06-22 12:42:01 +0100

    unauthenticated to authenticated route preservation added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  constructor(private router: Router) {}
+
+  public redirect() {
+    if (localStorage.getItem('redirectUrl') !== null) {
+      this.router.navigateByUrl(`${localStorage.getItem('redirectUrl')}`);
+      localStorage.clear();
+    }
+  }
+
+  ngOnInit(): void {
+    this.redirect();
+  }
+}
