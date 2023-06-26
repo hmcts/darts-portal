a06454d1564a3235652bd53e08675ef9d62a2ed6
Bredan Pires <bredan.pires@hmcts.net>
2023-06-26 16:16:36 +0100

    pre authentication landing screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -35,7 +35,7 @@ export function initAppFn(envService: AppConfigService) {
     TranscriptionsComponent,
     LoginComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, MatIconModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, MatIconModule, HttpClientModule, ReactiveFormsModule],
   providers: [
     {
       provide: APP_INITIALIZER,
--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,20 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
 import { AuthService } from 'src/app/services/auth/auth.service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
   let fakeAuthService: Partial<AuthService>;
+  let window: Window;
 
   beforeEach(() => {
     fakeAuthService = {
       isAuthenticated: jasmine.createSpy().and.resolveTo(true),
     };
 
+    window = {
+      location: {
+        href: '',
+      },
+    } as Window;
+
     TestBed.configureTestingModule({
       declarations: [LoginComponent],
-      providers: [{ provide: AuthService, useValue: fakeAuthService }],
+      providers: [
+        { provide: AuthService, useValue: fakeAuthService },
+        { provide: 'Window', useValue: window },
+      ],
+      imports: [ReactiveFormsModule],
     });
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
@@ -23,5 +35,12 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('#submit', () => {
+    it('should change url to /auth/login', () => {
+      component.submit();
+      expect(window.location.href).toBe('/auth/login');
+    });
   });
 });
