0434ef82fc51385c5f872b6c56268afefbed1602
Bredan Pires <bredan.pires@hmcts.net>
2023-06-27 11:49:59 +0100

    fix linting issues

--- cypress/e2e/smoke/smoke-spec.cy.js
+++ cypress/e2e/smoke/smoke-spec.cy.js
@@ -1,7 +1,7 @@
 describe('Smoke test', () => {
   it('should load portal login page', () => {
     cy.visit('/');
-    cy.get('.govuk-label').should('contain', "I'm an employee of HM Courts and Tribunals Service")
-    cy.get('.govuk-label').should('contain', "I work with the HM Courts and Tribunals Service")
+    cy.get('.govuk-label').should('contain', "I'm an employee of HM Courts and Tribunals Service");
+    cy.get('.govuk-label').should('contain', 'I work with the HM Courts and Tribunals Service');
   });
 });
