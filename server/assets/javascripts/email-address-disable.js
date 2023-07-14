5934e3983072e1a3ef721b0f49e008b837b40704
bpires2 <134072550+bpires2@users.noreply.github.com>
2023-07-14 15:40:31 +0100

    DMP 494 Forgotten Password Verify Code (#146)

--- server/assets/javascripts/email-address-disable.js
+++ server/assets/javascripts/email-address-disable.js
@@ -1,25 +1,23 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    (function () {
-        var origOpen = XMLHttpRequest.prototype.open;
-        XMLHttpRequest.prototype.open = function () {
-            const requestURL = arguments[1]
-            this.addEventListener('load', function (event) {
+(function () {
+    var origOpen = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function () {
+        const requestURL = arguments[1]
+        this.addEventListener('load', function (event) {
 
-                const isForSendCode = requestURL.indexOf('SendCode') > 0
-                const isSuccessful = this.readyState === 4 && this.responseText === `{"status":"200"}`
+            const isForSendCode = requestURL.indexOf('SendCode') > 0
+            const isSuccessful = this.readyState === 4 && this.responseText === `{"status":"200"}`
 
-                if (isForSendCode && isSuccessful) {
-                    setTimeout(() => {
-                        // hide intro
-                        document.getElementsByClassName('intro').item(0).style.display = 'none'
-                        // hide email label
-                        document.getElementById('email_label').style.display = 'none'
-                        // disable email input
-                        document.getElementById('email').disabled = true;
-                    }, 0)
-                }
-            });
-            origOpen.apply(this, arguments);
-        };
-    })();
-});
+            if (isForSendCode && isSuccessful) {
+                setTimeout(() => {
+                    // hide intro
+                    document.getElementsByClassName('intro').item(0).style.display = 'none'
+                    // hide email label
+                    document.getElementById('email_label').style.display = 'none'
+                    // disable email input
+                    document.getElementById('email').disabled = true;
+                }, 0)
+            }
+        });
+        origOpen.apply(this, arguments);
+    };
+})();
