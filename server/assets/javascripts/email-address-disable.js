1b39e5672b080d538ccd5f6ace6b1ef611e206c6
Bredan Pires <bredan.pires@hmcts.net>
2023-07-14 10:53:24 +0100

    styling and javascript changes

--- server/assets/javascripts/email-address-disable.js
+++ server/assets/javascripts/email-address-disable.js
@@ -1,18 +1,26 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    var input = document.getElementsByClassName('textInput')[0];
+    (function () {
+        var origOpen = XMLHttpRequest.prototype.open;
+        XMLHttpRequest.prototype.open = function () {
+            const requestURL = arguments[1]
+            this.addEventListener('load', function (event) {
+                console.log('request completed!', this.readyState, this.responseText);
 
-    button = document.getElementById('emailVerificationControl_but_verify_code');
+                const isForSendCode = requestURL.indexOf('SendCode') > 0
+                const isSuccessful = this.readyState === 4 && this.responseText === `{"status":"200"}`
 
-    if (button !== false) {
-        if (input.value !== '') {
-            input.disabled = true;
-        }
-    }
-
-
+                if (isForSendCode && isSuccessful) {
+                    setTimeout(() => {
+                        // hide intro
+                        document.getElementsByClassName('intro').item(0).style.display = 'none'
+                        // hide email label
+                        document.getElementById('email_label').style.display = 'none'
+                        // disable email input
+                        document.getElementById('email').disabled = true;
+                    }, 0)
+                }
+            });
+            origOpen.apply(this, arguments);
+        };
+    })();
 });
-
-
-
-
-
