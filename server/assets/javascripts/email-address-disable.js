793fb78da0c68a7544c55fc7ce08d854305259c6
bpires2 <134072550+bpires2@users.noreply.github.com>
2023-07-14 12:50:00 +0100

    DMP 494 Forgotten Password Verify Code (#145)

--- server/assets/javascripts/email-address-disable.js
+++ server/assets/javascripts/email-address-disable.js
@@ -1,17 +1,25 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    var input = document.getElementsByClassName('textInput')[0];
+    (function () {
+        var origOpen = XMLHttpRequest.prototype.open;
+        XMLHttpRequest.prototype.open = function () {
+            const requestURL = arguments[1]
+            this.addEventListener('load', function (event) {
 
-    button = document.getElementById('emailVerificationControl_but_verify_code');
+                const isForSendCode = requestURL.indexOf('SendCode') > 0
+                const isSuccessful = this.readyState === 4 && this.responseText === `{"status":"200"}`
 
-    button.addEventListener("click", () => {
-        if (input.value !== '') {
-            input.disabled = true;
-        }
-    })
-
+                if (isForSendCode && isSuccessful) {
+                    setTimeout(() => {
+                        // hide intro
+                        document.getElementsByClassName('intro').item(0).style.display = 'none'
+                        // hide email label
+                        document.getElementById('email_label').style.display = 'none'
+                        // disable email input
+                        document.getElementById('email').disabled = true;
+                    }, 0)
+                }
+            });
+            origOpen.apply(this, arguments);
+        };
+    })();
 });
-
-
-
-
-
