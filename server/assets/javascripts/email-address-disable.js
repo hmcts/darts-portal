0578b5a9ffa8d183fb109253c1896045d3109253
bpires2 <134072550+bpires2@users.noreply.github.com>
2023-07-17 15:27:14 +0100

    Dmp 494 forgotten password verify code (#149)

--- server/assets/javascripts/email-address-disable.js
+++ server/assets/javascripts/email-address-disable.js
@@ -1,13 +1,18 @@
 (function () {
+    console.log("Before XHR Prototype Open Override");
     var origOpen = XMLHttpRequest.prototype.open;
     XMLHttpRequest.prototype.open = function () {
-        const requestURL = arguments[1]
+        console.log("Inside XHR Prototype Open Override");
+        const requestURL = arguments[1];
+        console.log("Before Load event listener");
         this.addEventListener('load', function (event) {
-
+            console.log("After Load event listener");
             const isForSendCode = requestURL.indexOf('SendCode') > 0
             const isSuccessful = this.readyState === 4 && this.responseText === `{"status":"200"}`
 
+            console.log("Before If statement");
             if (isForSendCode && isSuccessful) {
+                console.log("Inside If Statement");
                 setTimeout(() => {
                     // hide intro
                     document.getElementsByClassName('intro').item(0).style.display = 'none'
@@ -18,6 +23,8 @@
                 }, 0)
             }
         });
+        console.log("Before applying origOpen.apply");
         origOpen.apply(this, arguments);
+        console.log("After applying origOpen.apply");
     };
 })();
