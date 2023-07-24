c6fc39c31cc8d1f6a1426391e78ced8efdeb8f74
Bredan Pires <bredan.pires@hmcts.net>
2023-07-24 12:45:05 +0100

    removing console.log, hiding email address text and changing link colour to blue

--- server/assets/javascripts/reset-password.js
+++ server/assets/javascripts/reset-password.js
@@ -1,30 +1,22 @@
 (function () {
-    console.log("Before XHR Prototype Open Override");
     var origOpen = XMLHttpRequest.prototype.open;
     XMLHttpRequest.prototype.open = function () {
-        console.log("Inside XHR Prototype Open Override");
         const requestURL = arguments[1];
-        console.log("Before Load event listener");
         this.addEventListener('load', function (event) {
-            console.log("After Load event listener");
             const isForSendCode = requestURL.indexOf('SendCode') > 0
             const isSuccessful = this.readyState === 4 && this.responseText === `{"status":"200"}`
 
-            console.log("Before If statement");
             if (isForSendCode && isSuccessful) {
-                console.log("Inside If Statement");
                 setTimeout(() => {
                     // hide intro
                     document.getElementsByClassName('intro').item(0).style.display = 'none'
                     // hide email label
                     document.getElementById('email_label').style.display = 'none'
                     // disable email input
-                    document.getElementById('email').disabled = true;
+                    document.getElementById('email').style.display = 'none';
                 }, 0)
             }
         });
-        console.log("Before applying origOpen.apply");
         origOpen.apply(this, arguments);
-        console.log("After applying origOpen.apply");
     };
 })();
