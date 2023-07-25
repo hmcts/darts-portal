0cbd3dcab66da0260b64cf461c64c46e23846fab
Dave Thompson <david.thompson@hmcts.net>
2023-07-25 12:50:34 +0100

    DMP-585 Password reset flow - code accepted screen (#170)

--- server/assets/javascripts/reset-password.js
+++ server/assets/javascripts/reset-password.js
@@ -5,17 +5,31 @@ document.getElementById('email').placeholder = '';
     XMLHttpRequest.prototype.open = function () {
         const requestURL = arguments[1];
         this.addEventListener('load', function (event) {
-            const isForSendCode = requestURL.indexOf('SendCode') > 0
-            const isSuccessful = this.readyState === 4 && this.responseText === `{"status":"200"}`
+            const isForSendCode = requestURL.indexOf('SendCode') > 0;
+            const isForVerifyCode = requestURL.indexOf('VerifyCode') > 0;
+            const isSuccessful = this.readyState === 4 && this.responseText === `{"status":"200"}`;
 
             if (isForSendCode && isSuccessful) {
                 setTimeout(() => {
                     // hide intro
-                    document.getElementsByClassName('intro').item(0).style.display = 'none'
+                    document.getElementsByClassName('intro').item(0).style.display = 'none';
                     // hide email label
-                    document.getElementById('email_label').style.display = 'none'
+                    document.getElementById('email_label').style.display = 'none';
                     // disable email input
                     document.getElementById('email').style.display = 'none';
+                }, 0)
+            }
+
+            if (isForVerifyCode && isSuccessful) {
+                setTimeout(() => {
+                    // show email
+                    $('#email').css('display', 'unset');
+                    // change heading
+                    $('.heading h1').text('Code accepted');
+                    // show continue button
+                    $('button#continue').css('display', 'unset');
+                    // move back button to correct position, due to continue button now being shown
+                    $('button#cancel').css('margin-left', '-88px');
                 }, 0)
             }
         });
