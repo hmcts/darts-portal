cb47a15fdd3dc0ef49be770062bdeec13a75f90a
LyleCGI <lyle.christine@hmcts.net>
2023-07-31 09:10:40 +0100

    dmp-507 : latest

--- server/assets/javascripts/search.js
+++ server/assets/javascripts/search.js
@@ -1,5 +1,5 @@
 /* clear search text box */
-document.getElementById('case-search').addEventListener('click', function(e) {
-  e.preventDefault(); // prevents the default action of the link
-  document.getElementById('case-search').value = ""; // clears the text box
+document.getElementById('clearLink').addEventListener('click', function(event) {
+  event.preventDefault();
+  document.getElementById('case-search').value = '';
 });
