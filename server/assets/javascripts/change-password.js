5028e50e50e166115587188d7c05d8265667c124
Bredan Pires <bredan.pires@hmcts.net>
2023-08-02 12:48:54 +0100

    added tab order fixes

--- server/assets/javascripts/change-password.js
+++ server/assets/javascripts/change-password.js
@@ -1,3 +1,22 @@
+document.getElementsByClassName('govuk-skip-link')[0].addEventListener("focusin", (event) => {
+    document.getElementById('cancel').style.top = "100px";
+})
+
+document.getElementsByClassName('govuk-skip-link')[0].addEventListener("focusout", (event) => {
+    document.getElementById('cancel').style.top = "60px";
+})
+
+function tabOrder() {
+    document.getElementsByClassName('govuk-skip-link')[0].tabIndex = 1;
+    document.getElementsByClassName('govuk-header__link govuk-header__link--homepage')[0].tabIndex = 2;
+    document.getElementById('cancel').tabIndex = 3;
+    document.getElementById('newPassword').tabIndex = 4;
+    document.getElementById('reenterPassword').tabIndex = 5;
+    document.getElementById('continue').tabIndex = 6;
+}
+
+tabOrder();
+
 // change heading
 $('.heading h1').text('Create a new password');
 // remove asterisks
