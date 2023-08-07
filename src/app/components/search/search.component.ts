695514c6f53d40dd00bb96e55ef4101fe347a9de
David Mann <david.mann1@hmcts.net>
2023-08-07 15:15:43 +0100

    Advanced UI changes

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,8 +1,46 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css'],
 })
-export class SearchComponent {}
+export class SearchComponent {
+  constructor(public fb: FormBuilder) {}
+  /*########### Template Driven Form ###########*/
+  searchForm = this.fb.group({
+    property: 'example',
+  });
+  // Submit Registration Form
+  onSubmit() {
+    alert(JSON.stringify(this.searchForm.value));
+  }
+
+  // specific-date id for specific radio
+  //date-range id for date range radio
+
+  onItemChange(type: String, evt: any) {
+    console.log('type', type);
+    console.log(' Event checked is : ', evt.target.checked);
+    if (evt.target) {
+      if (evt.target.checked) {
+      }
+    }
+  }
+
+  isRadioSelected(type: String) {
+    // return this.specific === type;
+    // if (type == 'speciifc') {
+    //   this.specificRadio = !this.specificRadio;
+    // }
+    // if (type == 'range') {
+    //   //Ref function to show range items
+    // }
+  }
+
+  hideDateItems(type: String) {
+    //date-range-items id for set of date inputs
+    //specific-date-items id for specific items
+  }
+}
