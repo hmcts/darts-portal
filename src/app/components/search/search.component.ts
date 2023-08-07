df47f103e4de038047902bd5f5e24f6afeaaf31f
David Mann <david.mann1@hmcts.net>
2023-08-07 15:56:17 +0100

    datepicker toggle changes for specific/date ranges

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 
 @Component({
@@ -8,6 +8,9 @@ import { FormBuilder } from '@angular/forms';
 })
 export class SearchComponent {
   constructor(public fb: FormBuilder) {}
+
+  specificShow: boolean = false;
+  rangeShow: boolean = false;
   /*########### Template Driven Form ###########*/
   searchForm = this.fb.group({
     property: 'example',
@@ -17,30 +20,28 @@ export class SearchComponent {
     alert(JSON.stringify(this.searchForm.value));
   }
 
-  // specific-date id for specific radio
-  //date-range id for date range radio
-
   onItemChange(type: String, evt: any) {
     console.log('type', type);
     console.log(' Event checked is : ', evt.target.checked);
     if (evt.target) {
       if (evt.target.checked) {
+        this.toggleRadioSelected(true, type);
+      } else {
+        this.toggleRadioSelected(false, type);
       }
     }
   }
 
-  isRadioSelected(type: String) {
-    // return this.specific === type;
-    // if (type == 'speciifc') {
-    //   this.specificRadio = !this.specificRadio;
-    // }
-    // if (type == 'range') {
-    //   //Ref function to show range items
-    // }
-  }
-
-  hideDateItems(type: String) {
-    //date-range-items id for set of date inputs
-    //specific-date-items id for specific items
+  toggleRadioSelected(selected: boolean, type: String) {
+    if (type == 'specific') {
+      //Show specific datepicker
+      this.specificShow = selected;
+      this.rangeShow = false;
+    }
+    if (type == 'range') {
+      //show range datepickers
+      this.rangeShow = selected;
+      this.specificShow = false;
+    }
   }
 }
