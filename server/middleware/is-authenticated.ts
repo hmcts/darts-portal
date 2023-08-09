30af184403d0200584fca877eb52668cc0be23cb
DavidTMann <d.mann@cgi.com>
2023-08-09 11:12:16 +0100

    Update is-authenticated.ts
    removal of flag

--- server/middleware/is-authenticated.ts
+++ server/middleware/is-authenticated.ts
@@ -1,8 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 
 export default (req: Request, res: Response, next: NextFunction): void => {
-  let login = true;
-
   if (!req.session?.securityToken?.accessToken) {
     res.redirect('/login');
   } else {
