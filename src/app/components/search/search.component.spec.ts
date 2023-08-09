e6aa88c10e87c2e0fc3ea30a30ee9813f2342a70
David Mann <david.mann1@hmcts.net>
2023-08-09 16:50:14 +0100

    unit tests for testing visibility toggle

--- src/app/components/search/search.component.spec.ts
+++ src/app/components/search/search.component.spec.ts
@@ -1,7 +1,6 @@
-import { ComponentFixture, TestBed, tick, flush, fakeAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
 import { SearchComponent } from './search.component';
 
 describe('SearchComponent', () => {
@@ -23,45 +22,30 @@ describe('SearchComponent', () => {
   });
 
   it('should change visibility of specific datepicker to true, and date range datepicker to false.', fakeAsync(() => {
-    spyOn(component, 'onItemChange');
-    spyOn(component, 'toggleRadioSelected');
-    // //Set specific date radio to checked
-    const specificRadioHtml: HTMLInputElement = fixture.debugElement.query(
+    spyOn(component, 'onItemChange').and.callThrough();
+    spyOn(component, 'toggleRadioSelected').and.callThrough();
+    const radio: HTMLInputElement = fixture.debugElement.query(
       By.css('input[name="specific-date-radio"]')
     ).nativeElement;
-    // const radio: HTMLInputElement = specificRadioHtml.nativeElement;
-    // specificRadioHtml.triggerEventHandler('change', { target: specificRadioHtml });
-    specificRadioHtml.dispatchEvent(new Event('change'));
-    fixture.detectChanges();
-    tick(1 * 60 * 1000); // move time 2 minutes in a fake way
-    flush();
+    radio.click();
+    tick();
 
     expect(component.onItemChange).toHaveBeenCalled();
     expect(component.toggleRadioSelected).toHaveBeenCalled();
     expect(component.specificShow).toBe(true);
-    // expect(component.rangeShow).toBe(false);
+    expect(component.rangeShow).toBe(false);
   }));
 
-  it('should change visibility of specific datepicker to true, and date range datepicker to false', fakeAsync(() => {
-    spyOn(component, 'onItemChange');
+  it('should change visibility of range datepicker to true, and specific datepicker to false', fakeAsync(() => {
+    spyOn(component, 'onItemChange').and.callThrough();
     spyOn(component, 'toggleRadioSelected').and.callThrough();
-    // //Set specific date radio to checked
-    // let options: DebugElement[] = fixture.debugElement.queryAll(By.css('input[name="specific-date-radio"]'));
-    // options[0].triggerEventHandler('change', { target: { checked: true } });
-    // fixture.detectChanges();
-
-    const mockEvent: Event = <Event>(<any>{
-      target: {
-        checked: true,
-      },
-    });
-
-    component.onItemChange('specific', mockEvent);
-    // tick(1 * 60 * 1000); // move time 2 minutes in a fake way
-    // flush();
+    const radio: HTMLInputElement = fixture.debugElement.query(By.css('input[name="date-range-radio"]')).nativeElement;
+    radio.click();
+    tick();
 
     expect(component.onItemChange).toHaveBeenCalled();
-    expect(component.specificShow).toBe(true);
-    // expect(component.rangeShow).toBe(false);
+    expect(component.toggleRadioSelected).toHaveBeenCalled();
+    expect(component.specificShow).toBe(false);
+    expect(component.rangeShow).toBe(true);
   }));
 });
