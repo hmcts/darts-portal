6d55adf7aae9ba0b6cf4e5a31dc6abb1e33c0486
David Mann <david.mann1@hmcts.net>
2023-08-09 10:02:17 +0100

    flag logic for login redirect

--- server/middleware/is-authenticated.ts
+++ server/middleware/is-authenticated.ts
@@ -1,7 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
 
 export default (req: Request, res: Response, next: NextFunction): void => {
-  if (!req.session.securityToken) {
+  let login = true;
+
+  if (req.session.securityToken) {
+    if (req.session.securityToken.accessToken) {
+      login = false;
+    }
+  }
+
+  if (login) {
     res.redirect('/login');
   } else {
     next();
