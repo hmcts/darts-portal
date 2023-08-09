e7d27b2c324132dff21379abfa9f78440424c806
DavidTMann <d.mann@cgi.com>
2023-08-09 11:08:10 +0100

    Update server/middleware/is-authenticated.ts
    if check now uses optional ? value on session and securityToken to check if accessToken exists before redirecting to login
    
    Co-authored-by: Dave Thompson <david.thompson@hmcts.net>

--- server/middleware/is-authenticated.ts
+++ server/middleware/is-authenticated.ts
@@ -3,13 +3,7 @@ import { Request, Response, NextFunction } from 'express';
 export default (req: Request, res: Response, next: NextFunction): void => {
   let login = true;
 
-  if (req.session.securityToken) {
-    if (req.session.securityToken.accessToken) {
-      login = false;
-    }
-  }
-
-  if (login) {
+  if (!req.session?.securityToken?.accessToken) {
     res.redirect('/login');
   } else {
     next();
