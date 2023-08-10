eb2b011800e691090b10f3d875d1ca2648b88255
Dave Thompson <davethompson21@gmail.com>
2023-08-10 09:15:45 +0100

    Reverting to using node_modules
    - it appears the third-party assets copying wasn't working
    - removing custom webpack build as it's no longer require
    d

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-// alias node_modules/govuk-frontend to govuk-frontend to allow imports to work with yarn PnP
-module.exports = {
-  resolve: {
-    alias: {
-      'node_modules/govuk-frontend': 'govuk-frontend',
-      'node_modules/@scottish-government/pattern-library': '@scottish-government/pattern-library',
-    },
-  },
-};
