b163b909de5bdda91dcb7179c9d03adca1a96f8a
DavidTMann <d.mann@cgi.com>
2023-08-10 11:56:35 +0100

    Dmp 508 (#195)

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
@@ -6,9 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.component.css'],
 })
 export class SearchComponent {
-  searchText = '';
+  dateInputType!: 'specific' | 'range';
+
+  form = new FormGroup({
+    searchText: new FormControl(),
+    courthouseText: new FormControl(),
+    courtroomText: new FormControl(),
+    defendantNameText: new FormControl(),
+    judgeNameText: new FormControl(),
+    keywordsText: new FormControl(),
+  });
+
+  // Submit Registration Form
+  onSubmit() {
+    alert(JSON.stringify(this.form.value));
+  }
+
+  toggleRadioSelected(type: 'specific' | 'range') {
+    this.dateInputType = type;
+  }
 
   clearSearch() {
-    this.searchText = '';
+    this.form.reset();
   }
 }
