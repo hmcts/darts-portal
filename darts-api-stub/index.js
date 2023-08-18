4686378007640dc93b6a0c20600531dc6369f974
David Mann <david.mann1@hmcts.net>
2023-08-18 12:11:23 +0100

    initial courthouses mock

--- darts-api-stub/index.js
+++ darts-api-stub/index.js
@@ -14,6 +14,8 @@ app.set('views', 'darts-api-stub/views');
 
 // stub out external user authentication
 app.use('/external-user', require('./authentication/external'));
+// stub out courthouses api
+app.use('/courthouses', require('./courthouses/courthouses'));
 // proxy non-stubbed routes to the actual API
 app.use(
   createProxyMiddleware({
