c7fbad8274aed1d04752e7ad4687848514fffd0e
Dean Bullock <dean.bullock@hmcts.net>
2023-08-24 09:48:41 +0100

    Remove redundant config

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,6 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
-  globalSetup: 'jest-preset-angular/global-setup',
   moduleDirectories: ['node_modules', '<rootDir>'],
   transformIgnorePatterns: ['node_modules/(?!@angular|@scottish-government)'],
 };
