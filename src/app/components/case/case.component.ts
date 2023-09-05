2e87c5237aa0bea2e6e724258ec291c5809413cc
Bredan Pires <bredan.pires@hmcts.net>
2023-09-05 15:45:33 +0100

    slimmer case.component.ts

--- src/app/components/case/case.component.ts
+++ src/app/components/case/case.component.ts
@@ -1,12 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CaseFileComponent } from './case-file/case-file.component';
 import { HearingResultsComponent } from './hearing-results/hearing-results.component';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { CaseService } from 'src/app/services/case/case.service';
-import { HearingData } from 'src/app/types/hearing';
-import { CaseFile } from 'src/app/types/case-file';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-case',
@@ -15,25 +12,11 @@ import { Observable } from 'rxjs';
   templateUrl: './case.component.html',
   styleUrls: ['./case.component.scss'],
 })
-export class CaseComponent implements OnInit {
-  public caseId!: string;
-  public caseFile$!: Observable<CaseFile>;
-  public hearings$!: Observable<HearingData[]>;
+export class CaseComponent {
+  private route = inject(ActivatedRoute);
+  private caseService = inject(CaseService);
 
-  constructor(private route: ActivatedRoute, private caseService: CaseService) {}
-
-  getCaseFile(): void {
-    this.caseId = this.route.snapshot.params.caseId;
-    this.caseFile$ = this.caseService.getCaseFile(this.caseId);
-  }
-
-  getCaseHearings(): void {
-    const caseId = this.route.snapshot.params.caseId;
-    this.hearings$ = this.caseService.getCaseHearings(caseId);
-  }
-
-  ngOnInit(): void {
-    this.getCaseFile();
-    this.getCaseHearings();
-  }
+  private caseId = this.route.snapshot.params.caseId;
+  public caseFile$ = this.caseService.getCaseFile(this.caseId);
+  public hearings$ = this.caseService.getCaseHearings(this.caseId);
 }
