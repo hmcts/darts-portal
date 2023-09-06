b432a43c3076d065b71287531a6845e19e282f1a
Dean Bullock <dean.bullock@hmcts.net>
2023-09-06 16:27:36 +0100

    fix tests

--- src/app/services/hearing/hearing.service.spec.ts
+++ src/app/services/hearing/hearing.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { HearingService } from './hearing.service';
@@ -6,7 +7,9 @@ describe('HearingService', () => {
   let service: HearingService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(HearingService);
   });
 
