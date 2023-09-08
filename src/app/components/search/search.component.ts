c6c99441cb38ea262820373e4d8a7d275728b155
David Mann <david.mann1@hmcts.net>
2023-09-08 11:23:24 +0100

    changes to include courthouse errors as its input is  dynamically generated

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -21,11 +21,11 @@ const FieldErrors: { [key: string]: { [key: string]: string } } = {
     required: 'You must enter a case number.',
   },
   courthouse: {
-    required: 'You must enter a courthouse.',
+    required: 'You must enter a courthouse, if courtroom is filled.',
   },
   courtroom: {
     required: 'You must enter a courtroom.', //example
-  }
+  },
 };
 
 @Component({
@@ -50,6 +50,7 @@ export class SearchComponent implements AfterViewChecked, AfterViewInit {
 
   form: FormGroup = new FormGroup({
     case_number: new FormControl('', Validators.required),
+    courthouse: new FormControl(),
     courtroom: new FormControl(),
     judge_name: new FormControl(),
     defendant_name: new FormControl(),
@@ -113,94 +114,6 @@ export class SearchComponent implements AfterViewChecked, AfterViewInit {
       return;
     }
 
-    // let valid = true;
-
-    // //AC2 - Searching a courtroom without courthouse
-    //  if (this.form.get('courtroom')?.value !== '') {
-    //   this.error = ERROR_MESSAGES.COURTROOM;
-    //   this.errorType = 'COURTROOM';
-    //   console.log(ERROR_MESSAGES.COURTROOM);
-    //   valid = false;
-    // }
-
-    // if (this.form.get('courthouse')?.value === undefined) {
-    //   const temp = this.form.get('courthouse')?.value;
-    //   console.log('courthouse is ' + temp)
-    //   valid = false;
-    // }
-
-    // const selectedDateInput = this.dateInputType.valueOf();
-
-    // //AC4 - Date range: start date is blank (same as AC5)
-    // if (selectedDateInput === 'range') {
-    //   if (this.form.get('date_from')?.value === '' || this.form.get('date_to')?.value !== '') {
-    //     this.errorType = 'START_DATE_MISSING';
-    //     this.error = ERROR_MESSAGES.START_DATE_MISSING;
-    //     console.log(ERROR_MESSAGES.START_DATE_MISSING);
-    //     valid = false;
-    //   }
-    // }
-
-    //AC5 - Date range: end date is blank
-    // if (selectedDateInput === 'range') {
-    //   if (this.form.get('date_from')?.value !== '' || this.form.get('date_to')?.value === '') {
-    //     console.log(ERROR_MESSAGES.END_DATE_MISSING);
-    //     this.errorType = 'END_DATE_MISSING';
-    //     this.error = ERROR_MESSAGES.END_DATE_MISSING;
-    //     valid = false;
-    //   }
-    // }
-
-    //If manual input, verify format, i.e. expects 08/08/2023
-    //AC6 - Invalid Date Format
-    // if (this.form.get('specific')?.value !== Date) {
-    //   this.errorType = 'INVALID_DATE';
-    //   this.error = ERROR_MESSAGES.INVALID_DATE;
-    //   console.log(ERROR_MESSAGES.INVALID_DATE);
-    //   valid = false;
-    // }
-
-    //AC3 - Entering a date that's in the future
-
-    // if (selectedDateInput === 'specific') {
-    //     const DATE_FROM = new Date(this.form.get('date_from')?.value).toLocaleString('en-GB');
-    //   console.log(DATE_FROM);
-    //   const DATE_TODAY = new Date().toLocaleString('en-GB');
-    //   console.log(DATE_TODAY);
-    //   if (DATE_FROM > DATE_TODAY) {
-    //     console.log(ERROR_MESSAGES.FUTURE_DATE)
-    //     this.errorType = 'FUTURE_DATE';
-    //     this.error = ERROR_MESSAGES.FUTURE_DATE;
-    //     } else {
-    //     valid = false;
-    //     return;
-    //   }
-    // }
-    //
-    // const DATE_FROM = new Date(this.form.get('date_from')?.value).toLocaleString('en-GB');
-    // function isDateInFuture(DATE_FROM: string): boolean {
-    //   const inputDate = new Date(DATE_FROM).toLocaleString('en-GB');
-    //   const currentDate = new Date().toLocaleString('en-GB');
-    //   return inputDate > currentDate;
-    // }
-
-    // isDateInFuture(DATE_FROM);
-
-    //AC3 - Entering a date that's in the future
-    // const TEST_DATE1 = new Date('02/09/2023').toLocaleString('en-GB');
-    // const TEST_DATE2 = new Date('02/11/2023').toLocaleString('en-GB');
-    // if (TEST_DATE1 > TEST_DATE2) {
-    //   console.log('Test date 1 is later in the year than test date 2');
-    //   } else {
-    //   console.log('Test date 2 is later in the year than test date 1')
-    // }
-
-    //const DATE_FROM_AGAIN = new Date(this.form.get('date_from')?.value);
-
-    // if(!valid) {
-    //   return;
-    // }
-
     this.caseService
       .getCasesAdvanced(
         this.form.get('case_number')?.value || '',
@@ -252,14 +165,28 @@ export class SearchComponent implements AfterViewChecked, AfterViewInit {
     return Object.keys(errors).map((errorType) => FieldErrors[fieldName][errorType]);
   }
 
+  //Gets courthouse errors, must be done in component as HTML input is dynamically generated
+  //via accessible-autocomplete module
+  getCourthouseErrors(): string[] {
+    //If courtroom is not empty, then courthouse must be filled
+    const courtroom = this.f['courtroom'].value;
+    const courthouse = (document.querySelector('input[name=courthouse]') as HTMLInputElement).value;
+
+    if (courtroom && courtroom !== '' && courthouse === '') {
+      this.f['courthouse'].setErrors({ required: true });
+      return [FieldErrors['courtroom']['required']];
+    }
+    return [];
+  }
+
   private generateErrorSummary(): ErrorSummaryEntry[] {
+    this.getCourthouseErrors();
+
     const formControls = this.f;
     return Object.keys(formControls)
       .filter((fieldId) => formControls[fieldId].errors)
       .map((fieldId) => this.getFieldErrorMessages(fieldId).map((message) => ({ fieldId, message })))
       .flat();
-
-    //Need custom block for non-formcontrol Courtrooms element
   }
 
   clearSearch() {
