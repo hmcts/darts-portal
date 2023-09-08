a5c56190eeb1ff9ae2971b66a2a8b827bc68af01
Dean Bullock <dean.bullock@hmcts.net>
2023-09-08 11:33:06 +0100

    Use login command

--- cypress/e2e/functional/my-audios-spec.cy.js
+++ cypress/e2e/functional/my-audios-spec.cy.js
@@ -1,12 +1,7 @@
+import './commands';
 describe('My audios', () => {
   beforeEach(() => {
-    cy.visit('/login');
-    cy.contains('I work with the HM Courts and Tribunals Service').click();
-    cy.contains('Continue').click();
-
-    cy.get('h1').should('contain', 'Stub login page');
-    cy.get('#login').click();
-    cy.get('h1').should('contain', 'Welcome to DARTS');
+    cy.login();
   });
 
   it('shows my audios', () => {
--- cypress/e2e/functional/my-transcriptions-spec.cy.js
+++ cypress/e2e/functional/my-transcriptions-spec.cy.js
@@ -1,12 +1,8 @@
+import './commands';
+
 describe('My transcriptions', () => {
   beforeEach(() => {
-    cy.visit('/login');
-    cy.contains('I work with the HM Courts and Tribunals Service').click();
-    cy.contains('Continue').click();
-
-    cy.get('h1').should('contain', 'Stub login page');
-    cy.get('#login').click();
-    cy.get('h1').should('contain', 'Welcome to DARTS');
+    cy.login();
   });
 
   it('shows my transcriptions', () => {
