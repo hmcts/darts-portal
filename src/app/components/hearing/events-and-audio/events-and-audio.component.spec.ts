3b18cea07f977a3d13cf857e54a2d3aa81dfb93f
Dean Bullock <125365979+deanbullock-hmcts@users.noreply.github.com>
2023-09-08 10:38:40 +0100

    DMP-823: Hearings event log only view (#275)
    Co-authored-by: Bredan Pires <bredan.pires@hmcts.net>
    Co-authored-by: David Mann <david.mann1@hmcts.net>

--- src/app/components/hearing/events-and-audio/events-and-audio.component.spec.ts
+++ src/app/components/hearing/events-and-audio/events-and-audio.component.spec.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Subscription } from 'rxjs';
+import { HearingEventTypeEnum } from 'src/app/types/enums';
 import { HearingAudio, HearingEvent, HearingAudioEventViewModel } from 'src/app/types/hearing-audio-event';
 import { EventsAndAudioComponent } from './events-and-audio.component';
 
@@ -7,15 +9,28 @@ describe('EventsAndAudioComponent', () => {
   let component: EventsAndAudioComponent;
   let fixture: ComponentFixture<EventsAndAudioComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [CommonModule, EventsAndAudioComponent],
-    }).compileComponents();
-  });
-
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [CommonModule, EventsAndAudioComponent],
+    });
     fixture = TestBed.createComponent(EventsAndAudioComponent);
     component = fixture.componentInstance;
+    component.audio = [
+      {
+        id: 1,
+        media_start_timestamp: '2023-07-31T14:32:24.620Z',
+        media_end_timestamp: '2023-07-31T14:32:24.620Z',
+      },
+    ];
+    component.events = [
+      {
+        id: 1,
+        timestamp: '2023-07-31T14:32:24.620Z',
+        name: 'Case called on',
+        text: 'Record: New Case',
+      },
+    ];
+
     fixture.detectChanges();
   });
 
@@ -23,40 +38,40 @@ describe('EventsAndAudioComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should initialize the table correctly', () => {
+  it('should construct the table by combining audio and events in order of timestamp', () => {
     const audio: HearingAudio[] = [
       {
         id: 1,
-        media_start_timestamp: '2023-07-31T14:32:24.620Z',
+        media_start_timestamp: '2023-07-31T10:00:01.620Z',
         media_end_timestamp: '2023-07-31T14:32:24.620Z',
       },
       {
         id: 2,
-        media_start_timestamp: '2022-07-31T14:32:24.620Z',
+        media_start_timestamp: '2023-07-31T10:00:04.620Z',
         media_end_timestamp: '2022-07-31T14:32:24.620Z',
       },
       {
         id: 3,
-        media_start_timestamp: '2023-07-31T14:32:24.620Z',
+        media_start_timestamp: '2023-07-31T10:00:06.620Z',
         media_end_timestamp: '2023-07-31T14:32:24.620Z',
       },
     ];
     const events: HearingEvent[] = [
       {
-        id: 1,
-        timestamp: '2023-07-31T14:32:24.620Z',
+        id: 8,
+        timestamp: '2023-07-31T10:00:02.620Z',
         name: 'Case called on',
         text: 'Record: New Case',
       },
       {
-        id: 2,
-        timestamp: '2023-07-31T14:32:24.620Z',
+        id: 9,
+        timestamp: '2023-07-31T10:00:03.620Z',
         name: 'Case called on',
         text: 'Record: New Case',
       },
       {
-        id: 3,
-        timestamp: '2023-07-31T14:32:24.620Z',
+        id: 10,
+        timestamp: '2023-07-31T10:00:07.620Z',
         name: 'Case called on',
         text: 'Record: New Case',
       },
@@ -65,47 +80,50 @@ describe('EventsAndAudioComponent', () => {
     component.audio = audio;
     component.events = events;
 
-    component.ngOnInit();
+    component.ngOnChanges();
 
     const expectedTable: HearingAudioEventViewModel[] = [
       {
-        id: 1,
-        media_start_timestamp: '2023-07-31T14:32:24.620Z',
+        id: 10,
+        timestamp: '2023-07-31T10:00:07.620Z',
+        name: 'Case called on',
+        text: 'Record: New Case',
+        type: 'event',
+      },
+      {
+        id: 3,
+        media_start_timestamp: '2023-07-31T10:00:06.620Z',
         media_end_timestamp: '2023-07-31T14:32:24.620Z',
+        timestamp: '2023-07-31T10:00:06.620Z',
         type: 'audio',
       },
       {
         id: 2,
-        media_start_timestamp: '2022-07-31T14:32:24.620Z',
+        media_start_timestamp: '2023-07-31T10:00:04.620Z',
         media_end_timestamp: '2022-07-31T14:32:24.620Z',
+        timestamp: '2023-07-31T10:00:04.620Z',
         type: 'audio',
       },
       {
-        id: 3,
-        media_start_timestamp: '2023-07-31T14:32:24.620Z',
-        media_end_timestamp: '2023-07-31T14:32:24.620Z',
-        type: 'audio',
+        id: 9,
+        timestamp: '2023-07-31T10:00:03.620Z',
+        name: 'Case called on',
+        text: 'Record: New Case',
+        type: 'event',
+      },
+      {
+        id: 8,
+        timestamp: '2023-07-31T10:00:02.620Z',
+        name: 'Case called on',
+        text: 'Record: New Case',
+        type: 'event',
       },
       {
         id: 1,
-        timestamp: '2023-07-31T14:32:24.620Z',
-        name: 'Case called on',
-        text: 'Record: New Case',
-        type: 'event',
-      },
-      {
-        id: 2,
-        timestamp: '2023-07-31T14:32:24.620Z',
-        name: 'Case called on',
-        text: 'Record: New Case',
-        type: 'event',
-      },
-      {
-        id: 3,
-        timestamp: '2023-07-31T14:32:24.620Z',
-        name: 'Case called on',
-        text: 'Record: New Case',
-        type: 'event',
+        media_start_timestamp: '2023-07-31T10:00:01.620Z',
+        media_end_timestamp: '2023-07-31T14:32:24.620Z',
+        timestamp: '2023-07-31T10:00:01.620Z',
+        type: 'audio',
       },
     ];
 
@@ -113,7 +131,6 @@ describe('EventsAndAudioComponent', () => {
   });
 
   it('should toggle row selection', () => {
-    // Mock a row for testing
     const row: HearingAudioEventViewModel = {
       id: 1,
       media_start_timestamp: '2023-07-31T14:32:24.620Z',
@@ -121,31 +138,66 @@ describe('EventsAndAudioComponent', () => {
       type: 'audio',
     };
 
-    // Call the toggleRowSelection method
     component.toggleRowSelection(row);
 
-    // Expect the row to be in the selectedRows array
     expect(component.selectedRows).toContain(row);
 
-    // Call the toggleRowSelection method again to deselect the row
     component.toggleRowSelection(row);
 
-    // Expect the row to be removed from the selectedRows array
     expect(component.selectedRows).not.toContain(row);
   });
 
   it('should check if a row is selected', () => {
-    // Mock a row for testing
     const row: HearingAudioEventViewModel = {
       id: 1,
       media_start_timestamp: '2023-07-31T14:32:24.620Z',
       media_end_timestamp: '2023-07-31T14:32:24.620Z',
       type: 'audio',
     };
-    // Add the row to the selectedRows array
+
     component.selectedRows.push(row);
 
-    // Call the isRowSelected method and expect it to return true
     expect(component.isRowSelected(row)).toBeTruthy();
+  });
+
+  it('should filter rows correctly', () => {
+    const eventRow: HearingAudioEventViewModel = { id: 1, type: HearingEventTypeEnum.Event };
+    component.table = [
+      { id: 2, type: HearingEventTypeEnum.Audio },
+      eventRow,
+      { id: 3, type: HearingEventTypeEnum.Audio },
+    ];
+
+    component.onFilterChanged('event');
+    expect(component.filteredTable).toEqual([eventRow]);
+
+    component.onFilterChanged('all');
+    expect(component.filteredTable).toEqual(component.table);
+  });
+
+  it('should unsubscribe on ngOnDestroy', () => {
+    const subSpy = new Subscription();
+    jest.spyOn(subSpy, 'unsubscribe');
+    component.subs.push(subSpy);
+
+    component.ngOnDestroy();
+
+    expect(subSpy.unsubscribe).toHaveBeenCalled();
+  });
+
+  it('should filter by events', () => {
+    component.form.get('selectedOption')?.setValue('event');
+
+    fixture.detectChanges();
+
+    expect(component.filteredTable).toEqual(component.table.filter((row) => row.type === HearingEventTypeEnum.Event));
+  });
+
+  it('should filter by all', () => {
+    component.form.get('selectedOption')?.setValue('all');
+
+    fixture.detectChanges();
+
+    expect(component.filteredTable).toEqual(component.table);
   });
 });
