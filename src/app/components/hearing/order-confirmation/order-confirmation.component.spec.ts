fe73691bc845226d03508921ce9b03d338f2040b
Dean Bullock <dean.bullock@hmcts.net>
2023-09-20 14:27:11 +0100

    Fix tests

--- src/app/components/hearing/order-confirmation/order-confirmation.component.spec.ts
+++ src/app/components/hearing/order-confirmation/order-confirmation.component.spec.ts
@@ -1,17 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CaseData } from '@darts-types/index';
 
 import { OrderConfirmationComponent } from './order-confirmation.component';
 
 describe('OrderConfirmationComponent', () => {
   let component: OrderConfirmationComponent;
   let fixture: ComponentFixture<OrderConfirmationComponent>;
+  const MOCK_CASE: CaseData = {
+    case_id: 1,
+    case_number: 'C20220620001',
+    courthouse: 'Swansea',
+    defendants: ['Defendant Dave'],
+    judges: ['Judge Judy'],
+    reporting_restriction: 'Section 4(2) of the Contempt of Court Act 1981',
+    hearings: [
+      {
+        id: 1,
+        date: '2023-08-10',
+        courtroom: '1',
+        judges: ['Judge Judy'],
+        transcript_count: 0,
+      },
+    ],
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [OrderConfirmationComponent]
+      imports: [OrderConfirmationComponent],
     });
     fixture = TestBed.createComponent(OrderConfirmationComponent);
     component = fixture.componentInstance;
+    component.case = MOCK_CASE;
     fixture.detectChanges();
   });
 
--- src/app/components/hearing/order-summary/order-summary.component.spec.ts
+++ src/app/components/hearing/order-summary/order-summary.component.spec.ts
@@ -1,17 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CaseData } from '@darts-types/index';
 
 import { OrderSummaryComponent } from './order-summary.component';
 
 describe('OrderSummaryComponent', () => {
   let component: OrderSummaryComponent;
   let fixture: ComponentFixture<OrderSummaryComponent>;
+  const MOCK_CASE: CaseData = {
+    case_id: 1,
+    case_number: 'C20220620001',
+    courthouse: 'Swansea',
+    defendants: ['Defendant Dave'],
+    judges: ['Judge Judy'],
+    reporting_restriction: 'Section 4(2) of the Contempt of Court Act 1981',
+    hearings: [
+      {
+        id: 1,
+        date: '2023-08-10',
+        courtroom: '1',
+        judges: ['Judge Judy'],
+        transcript_count: 0,
+      },
+    ],
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [OrderSummaryComponent]
+      imports: [OrderSummaryComponent],
     });
     fixture = TestBed.createComponent(OrderSummaryComponent);
     component = fixture.componentInstance;
+    component.case = MOCK_CASE;
     fixture.detectChanges();
   });
 
