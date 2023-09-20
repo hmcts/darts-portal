23dff1dc0a9b88da1481c68957d3c8a75127eb85
Dave Thompson <david.thompson@hmcts.net>
2023-09-20 15:30:11 +0100

    DMP-912 case search functional tests (#315)

--- src/app/components/case/case-file/case-file.component.ts
+++ src/app/components/case/case-file/case-file.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CaseData } from '@darts-types/index';
+import { JoinPipe } from '@pipes/join';
 
 @Component({
   selector: 'app-case-file',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, JoinPipe],
   templateUrl: './case-file.component.html',
   styleUrls: ['./case-file.component.scss'],
 })
