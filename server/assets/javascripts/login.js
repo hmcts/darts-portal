dd0b34de005d23bcf1e7ece540db0e2476de7f32
DavidTMann <d.mann@cgi.com>
2023-09-21 16:39:54 +0100

    DMP-995/999/1000/1001 - accessibility changes (#329)

--- server/assets/javascripts/login.js
+++ server/assets/javascripts/login.js
@@ -2,6 +2,14 @@ function moveForgotPasswordLink() {
 	$('.entry-item:nth-child(2)').after($('#forgotPassword'))
 }
 
+//File used strictly for updating HTML for accessibility purposes
+function loginAccessibility(){
+  //Set aria-required attribute to email input in azure b2c login form
+  if (document.getElementsById('email')) {
+      document.getElementsById('email').setAttribute("aria-required", "true");
+  }
+}
+
 function displayErrors() {
   removeErrors();
   addItemLevelErrorClasses();
@@ -10,6 +18,7 @@ function displayErrors() {
 }
 
 moveForgotPasswordLink();
+loginAccessibility();
 $('button#next').click(displayErrors);
 
 // wait a second before trying to do this, in case the JS in head isn't loaded yet
