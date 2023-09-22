f754df48dec7811fb77e1476d158efdfbac2b197
David Mann <david.mann1@hmcts.net>
2023-09-22 09:11:10 +0100

    refer to single element for aria-required email input

--- server/assets/javascripts/login.js
+++ server/assets/javascripts/login.js
@@ -5,8 +5,8 @@ function moveForgotPasswordLink() {
 //File used strictly for updating HTML for accessibility purposes
 function loginAccessibility(){
   //Set aria-required attribute to email input in azure b2c login form
-  if (document.getElementsById('email')) {
-      document.getElementsById('email').setAttribute("aria-required", "true");
+  if (document.getElementById('email')) {
+      document.getElementById('email').setAttribute("aria-required", "true");
   }
 }
 
