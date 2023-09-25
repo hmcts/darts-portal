ac8d7ece6b7422a70de0f6ea11be90b254afd5fc
Dean Bullock <dean.bullock@hmcts.net>
2023-09-25 11:33:19 +0100

    Create data table poc

---
+++ src/app/components/common/data-table/data-table.component.ts
@@ -0,0 +1,97 @@
+import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SortingInterface } from '@components/search/results/results.component';
+import { PaginationComponent } from '@common/pagination/pagination.component';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-data-table',
+  standalone: true,
+  imports: [CommonModule, PaginationComponent, RouterLink],
+  templateUrl: './data-table.component.html',
+  styleUrls: ['./data-table.component.scss'],
+})
+export class DataTableComponent implements OnChanges {
+  @Input() rows: any[] = [];
+  @Input() columns: any[] = [];
+  @Input() caption = '';
+
+  // TO DO:
+  @Input() showCheckboxColumn = false;
+  @Input() showPagination = true;
+  @Output() rowSelectionChanged = new EventEmitter<any[]>();
+  selectedRows: boolean[] = [];
+
+  pagedRows: any[] = [];
+
+  currentPage = 1;
+  itemsPerPage = 10;
+  pageLimit = 5;
+
+  sorting: SortingInterface = {
+    column: '',
+    order: 'asc',
+  };
+
+  sortNull() {
+    return 0;
+  }
+
+  ngOnChanges(): void {
+    this.sorting.column = '';
+    this.currentPage = 1;
+    this.updatePagedData();
+  }
+
+  onPageChanged(page: number): void {
+    this.currentPage = page;
+    this.updatePagedData();
+  }
+
+  sortTable(column: any): void {
+    this.sorting = {
+      column: column,
+      order: this.isDescSorting(column) ? 'asc' : 'desc',
+    };
+
+    this.rows.sort((a, b) => {
+      const valueA: string | undefined = a[column];
+      const valueB: string | undefined = b[column];
+
+      if (typeof valueA === 'string' && typeof valueB === 'string') {
+        return this.compareStrings(column, valueA, valueB);
+      } else {
+        return 0;
+      }
+    });
+
+    this.updatePagedData();
+  }
+
+  compareStrings(column: any, a: string, b: string): number {
+    return this.isAscSorting(column) ? a.localeCompare(b) : b.localeCompare(a);
+  }
+
+  isDescSorting(column: any): boolean {
+    return this.sorting.column === column && this.sorting.order === 'desc';
+  }
+
+  isAscSorting(column: any): boolean {
+    return this.sorting.column === column && this.sorting.order === 'asc';
+  }
+
+  getAriaSort(column: any): 'ascending' | 'descending' | 'none' {
+    if (this.sorting.column === column) {
+      return this.isAscSorting(column) ? 'ascending' : 'descending';
+    }
+    return 'none';
+  }
+
+  private updatePagedData(): void {
+    this.pagedRows = this.paginate(this.rows, this.pageLimit, this.currentPage);
+  }
+
+  private paginate(array: any[], pageSize: number, currentPage: number) {
+    return array.slice((currentPage - 1) * pageSize, currentPage * pageSize);
+  }
+}
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,22 @@
+import { CaseService } from 'src/app/services/case/case.service';
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { DataTableComponent } from '@common/data-table/data-table.component';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { map } from 'rxjs';
+import { DateTimeService } from '@services/datetime/datetime.service';
+import { CaseData } from '@darts-types/case';
+import { HearingData } from '@darts-types/hearing';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, DataTableComponent, AsyncPipe, CommonModule],
 })
 export class HomeComponent implements OnInit {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private caseService: CaseService) {}
 
   public redirect() {
     if (localStorage.getItem('redirectUrl') !== null) {
@@ -18,7 +25,69 @@ export class HomeComponent implements OnInit {
     }
   }
 
+  caseRows$ = this.caseService.getCasesAdvanced({ case_number: 'ALL' }).pipe(
+    map((cases) =>
+      cases.map((c) => {
+        return {
+          id: c.case_id,
+          caseNumber: c.case_number,
+          courthouse: c.courthouse,
+          courtroom: this.getHearingsValue(c, 'courtroom'),
+          judges: this.getNameValue(c.judges),
+          defendants: this.getNameValue(c.defendants),
+          reporting_restriction: c.reporting_restriction,
+        };
+      })
+    )
+  );
+
+  columns = [
+    { name: 'Case ID', prop: 'caseNumber', sortable: true, link: '/case' },
+    { name: 'Courthouse', prop: 'courthouse', sortable: true },
+    { name: 'Courtroom', prop: 'courtroom', sortable: false },
+    { name: 'Judge(s)', prop: 'judges', sortable: false },
+    { name: 'Defendants(s)', prop: 'defendants', sortable: false },
+  ];
+
   ngOnInit(): void {
     this.redirect();
+  }
+
+  //Fetches correct display value for defendants and judges
+  getNameValue(arr: string[] | undefined): string {
+    if (arr) {
+      if (arr.length === 0) {
+        return '';
+      } else {
+        if (arr.length < 2) {
+          return arr[0];
+        } else {
+          return 'Multiple';
+        }
+      }
+    } else {
+      return '';
+    }
+  }
+
+  //Fetches correct display value for dates and courtrooms
+  getHearingsValue(c: CaseData, key: keyof HearingData): string {
+    if (!c.hearings || c.hearings.length === 0) {
+      return '';
+    } else {
+      if (c.hearings.length < 2) {
+        if (key === 'date') {
+          return this.getDateFormat(c.hearings[0][key]);
+        } else {
+          return c.hearings[0][key] as string;
+        }
+      } else {
+        return 'Multiple';
+      }
+    }
+  }
+
+  private getDateFormat(d: string): string {
+    return DateTimeService.getdddDMMMYYYY(d);
   }
 }
