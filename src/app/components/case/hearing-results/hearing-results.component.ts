e2993bc9150c94a220130ee208ce69fc6c80e54e
Dean Bullock <dean.bullock@hmcts.net>
2023-09-29 13:13:15 +0100

    Add tabs component to hearing-results

--- src/app/components/case/hearing-results/hearing-results.component.ts
+++ src/app/components/case/hearing-results/hearing-results.component.ts
@@ -1,20 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { RouterLink, ActivatedRoute } from '@angular/router';
+import { TabsComponent } from '@common/tabs/tabs.component';
 import { Hearing } from '@darts-types/index';
 
 @Component({
   selector: 'app-hearing-results',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, TabsComponent],
   templateUrl: './hearing-results.component.html',
   styleUrls: ['./hearing-results.component.scss'],
 })
 export class HearingResultsComponent {
   caseId: number;
   @Input() hearings: Hearing[] = [];
+  tabs: string[];
 
   constructor(private route: ActivatedRoute) {
     this.caseId = this.route.snapshot.params.caseId;
+    this.tabs = ['Hearings', 'Transcripts'];
   }
 }
