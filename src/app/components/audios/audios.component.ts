eaf261909d9ac8437941dfa45733e9959f41aa19
Dean Bullock <dean.bullock@hmcts.net>
2023-09-29 13:16:30 +0100

    Add tabs and title to your audio screen

--- src/app/components/audios/audios.component.ts
+++ src/app/components/audios/audios.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { TabsComponent } from '@common/tabs/tabs.component';
 
 @Component({
   selector: 'app-audios',
   templateUrl: './audios.component.html',
   styleUrls: ['./audios.component.scss'],
   standalone: true,
+  imports: [TabsComponent],
 })
-export class AudiosComponent {}
+export class AudiosComponent {
+  tabs = ['Current', 'Expired'];
+}
