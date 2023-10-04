5404f733cc313c1e365715b869eb748be4ecd9d4
Dean Bullock <dean.bullock@hmcts.net>
2023-10-04 16:17:06 +0100

    Add page limit to ready table

--- pa11y/pa11y.ts
+++ pa11y/pa11y.ts
@@ -2,7 +2,16 @@ import pa11y from 'pa11y';
 
 import { testUrl } from './config';
 
-export const pathsToTest = ['/', '/login', '/search', '/audios', '/transcriptions', '/case/1', '/case/1/hearing/1', '/page-not-found'];
+export const pathsToTest = [
+  '/',
+  '/login',
+  '/search',
+  '/audios',
+  '/transcriptions',
+  '/case/1',
+  '/case/1/hearing/1',
+  '/page-not-found',
+];
 
 async function runPa11y() {
   try {
