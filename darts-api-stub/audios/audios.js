2c237fabb2e567e6066c742eb89660537c541616
David Mann <david.mann1@hmcts.net>
2023-10-09 10:46:32 +0100

    uncommented post audio req

--- darts-api-stub/audios/audios.js
+++ darts-api-stub/audios/audios.js
@@ -2,15 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
-// const audioRequestOne = {
-//   request_id: 1234,
-//   case_id: 'T4565443',
-//   courthouse_name: 'Swansea',
-//   defendants: ['Derek Defender'],
-//   hearing_date: '2023-09-20',
-//   start_time: '2023-09-20T09:00:00Z',
-//   end_time: '2023-09-20T10:00:00Z',
-// };
+const audioRequestOne = {
+  request_id: 1234,
+  case_id: 'T4565443',
+  courthouse_name: 'Swansea',
+  defendants: ['Derek Defender'],
+  hearing_date: '2023-09-20',
+  start_time: '2023-09-20T09:00:00Z',
+  end_time: '2023-09-20T10:00:00Z',
+};
 
 const audioRequestMulti = [
   {
