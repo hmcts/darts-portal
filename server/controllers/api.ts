5f020515c488c39014e40ff618046ce70075f9af
Dave Thompson <david.thompson@hmcts.net>
2023-10-19 13:38:34 +0100

    Adding some logs to API proxy (#437)

--- server/controllers/api.ts
+++ server/controllers/api.ts
@@ -13,11 +13,17 @@ function proxyMiddleware() {
     },
     logLevel: 'debug',
     onProxyReq: (proxyReq, req) => {
+      console.log('onProxyReq::proxyReq', req.path, proxyReq);
+      console.log('onProxyReq::req', req.path, req);
       if (req.session.securityToken) {
         if (req.session.securityToken.accessToken) {
           proxyReq.setHeader('Authorization', `Bearer ${req.session.securityToken.accessToken}`);
         }
       }
+    },
+    onProxyRes: (proxyRes, req, res) => {
+      console.log('onProxyRes::proxyRes', req.path, proxyRes);
+      console.log('onProxyRes::res', req.path, res);
     },
   });
 }
