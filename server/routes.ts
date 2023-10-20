3694d5092d97f9469be1586c24cb65fb79b14609
Dave Thompson <david.thompson@hmcts.net>
2023-10-20 12:08:29 +0100

    Adding bits for testing audio streaming issue (#453)

--- server/routes.ts
+++ server/routes.ts
@@ -2,6 +2,8 @@ import express, { NextFunction, Request, Response } from 'express';
 import type { Router } from 'express';
 import { apiController, appController, authController, authenticationController, userController } from './controllers';
 import { isAuthenticated } from './middleware';
+import axios from 'axios';
+import config from 'config';
 
 export default (disableAuthentication = false): Router => {
   const router = express.Router();
@@ -9,6 +11,28 @@ export default (disableAuthentication = false): Router => {
   const checkAuthenticated = disableAuthentication
     ? (req: Request, res: Response, next: NextFunction) => next()
     : isAuthenticated;
+
+  // START:
+  // This is for testing audio preview without http-middleware-proxy
+  // To be removed afterwards.
+  router.use('/api-test', checkAuthenticated, async (req, res) => {
+    try {
+      console.log('API-TEST::before HTTP request', new Date());
+      const response = await axios.get(`${config.get('darts-api.url')}${req.path.replace('api-test', '')}`, {
+        headers: {
+          Authorization: `Bearer ${req.session?.securityToken?.accessToken}`,
+        },
+      });
+      console.log('API-TEST::after HTTP request', new Date());
+      res.set(response.headers);
+      res.status(response.status);
+      res.send(response.data);
+    } catch (err) {
+      console.error('API-TEST::error', new Date());
+      res.status(500).send(err);
+    }
+  });
+  // END
 
   // authenticated routes
   router.use('/api', checkAuthenticated, apiController.init());
