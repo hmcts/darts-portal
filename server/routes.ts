3fadcdd12c24dedc75578dd8b6ff18d8b4f44de8
Dave Thompson <david.thompson@hmcts.net>
2023-10-20 15:04:42 +0100

    Amending logging for test audio API request (#454)

--- server/routes.ts
+++ server/routes.ts
@@ -15,22 +15,25 @@ export default (disableAuthentication = false): Router => {
   // START:
   // This is for testing audio preview without http-middleware-proxy
   // To be removed afterwards.
-  router.use('/api-test', checkAuthenticated, async (req, res) => {
-    try {
-      console.log('API-TEST::before HTTP request', new Date());
-      const response = await axios.get(`${config.get('darts-api.url')}${req.path.replace('api-test', '')}`, {
+  router.use('/api-test', checkAuthenticated, (req, res) => {
+    console.log('API-TEST::before HTTP request', req.path, new Date());
+    axios
+      .get(`${config.get('darts-api.url')}${req.path.replace('api-test', '')}`, {
         headers: {
           Authorization: `Bearer ${req.session?.securityToken?.accessToken}`,
         },
+      })
+      .then((response) => {
+        console.log('API-TEST::after HTTP request', req.path, new Date(), 'status', response.status);
+        console.log('API-TEST::after HTTP request', req.path, new Date(), 'headers', response.headers);
+        res.set(response.headers);
+        res.status(response.status);
+        res.send(response.data);
+      })
+      .catch((err) => {
+        console.error('API-TEST::error', new Date());
+        res.status(500).send(err);
       });
-      console.log('API-TEST::after HTTP request', new Date());
-      res.set(response.headers);
-      res.status(response.status);
-      res.send(response.data);
-    } catch (err) {
-      console.error('API-TEST::error', new Date());
-      res.status(500).send(err);
-    }
   });
   // END
 
