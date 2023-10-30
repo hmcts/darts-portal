e19994973a5e82bb7d9a4d3381e0086f755bbd66
David Mann <david.mann1@hmcts.net>
2023-10-30 14:49:22 +0000

    functional test & stub update

--- darts-api-stub/audios/audios.js
+++ darts-api-stub/audios/audios.js
@@ -87,6 +87,18 @@ const audioRequestMulti = [
     last_accessed_ts: '2023-08-23T09:00:00Z',
   },
   {
+    case_id: 7,
+    media_request_id: 12376,
+    case_number: 'T20200334',
+    courthouse_name: 'Manchester',
+    hearing_date: '2023-10-04',
+    media_request_start_ts: '2023-08-21T09:00:00Z',
+    media_request_end_ts: '2023-08-21T10:00:00Z',
+    media_request_expiry_ts: '2023-08-23T09:00:00Z',
+    media_request_status: 'COMPLETED',
+    last_accessed_ts: '2023-08-23T09:00:00Z',
+  },
+  {
     case_id: 8,
     media_request_id: 12342,
     case_number: 'T2020011820',
