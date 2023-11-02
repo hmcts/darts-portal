1f87ae287cd146dc126715cb53a6ce06a805d5d4
Dave Thompson <davethompson21@gmail.com>
2023-11-02 10:42:00 +0000

    Adding case loading icons and basic error handling
    - includes 403, 404, 400 and 500 errors resulting from the case route

---
+++ src/app/components/common/forbidden/forbidden.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-forbidden',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './forbidden.component.html',
+  styleUrls: ['./forbidden.component.scss'],
+})
+export class ForbiddenComponent {}
