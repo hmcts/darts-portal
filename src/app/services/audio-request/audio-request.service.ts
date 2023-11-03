642db90fd17351bcd5dd25240b6d5b6bd4f36ff2
joshw-hmcts <149702321+joshw-hmcts@users.noreply.github.com>
2023-11-03 15:14:55 +0000

    DMP-1383 - Inject user_id header for all DARTS API requests (#494)
    Co-authored-by: Josh Windus <josh.windus@cgi.com>

--- src/app/services/audio-request/audio-request.service.ts
+++ src/app/services/audio-request/audio-request.service.ts
@@ -40,16 +40,11 @@ export class AudioRequestService {
   );
 
   getAudioRequests(expired: boolean): Observable<UserAudioRequest[]> {
-    return this.userProfile$.pipe(
-      switchMap((userState) => {
-        return this.http
-          .get<UserAudioRequest[]>(`api/audio-requests`, {
-            headers: { user_id: userState.userId.toString() },
-            params: { expired },
-          })
-          .pipe(map((requests) => requests.map((r) => ({ ...r, hearing_date: r.hearing_date + 'T00:00:00Z' }))));
+    return this.http
+      .get<UserAudioRequest[]>(`api/audio-requests`, {
+        params: { expired },
       })
-    );
+      .pipe(map((requests) => requests.map((r) => ({ ...r, hearing_date: r.hearing_date + 'T00:00:00Z' }))));
   }
 
   deleteAudioRequests(mediaRequestId: number): Observable<HttpResponse<Response>> {
@@ -84,15 +79,7 @@ export class AudioRequestService {
   }
 
   getUnreadCount(): Observable<number> {
-    return this.userProfile$.pipe(
-      switchMap((userState) =>
-        this.http
-          .get<{ count: number }>(UNREAD_AUDIO_COUNT_PATH, {
-            headers: { user_id: userState.userId.toString() },
-          })
-          .pipe(map((res) => res.count))
-      )
-    );
+    return this.http.get<{ count: number }>(UNREAD_AUDIO_COUNT_PATH, {}).pipe(map((res) => res.count));
   }
 
   private updateUnread(audioRequests: UserAudioRequest[]) {
