c86519ad793160de07d048f3328bd0824d9acd5d
Dave Thompson <davethompson21@gmail.com>
2023-11-03 11:23:32 +0000

    Fixing unit tests

--- src/app/components/common/forbidden/forbidden.component.spec.ts
+++ src/app/components/common/forbidden/forbidden.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppConfigService } from '@services/app-config/app-config.service';
 
 import { ForbiddenComponent } from './forbidden.component';
 
@@ -6,9 +7,20 @@ describe('ForbiddenComponent', () => {
   let component: ForbiddenComponent;
   let fixture: ComponentFixture<ForbiddenComponent>;
 
+  const appConfigServiceMock = {
+    getAppConfig: () => ({
+      appInsightsKey: 'XXXXXXXX',
+      support: {
+        name: 'DARTS support',
+        emailAddress: 'support@darts',
+      },
+    }),
+  };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [ForbiddenComponent],
+      providers: [{ provide: AppConfigService, useValue: appConfigServiceMock }],
     });
     fixture = TestBed.createComponent(ForbiddenComponent);
     component = fixture.componentInstance;
--- src/app/components/error/internal-server/internal-error.component.spec.ts
+++ src/app/components/error/internal-server/internal-error.component.spec.ts
@@ -1,13 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppConfigService } from '@services/app-config/app-config.service';
 import { InternalErrorComponent } from './internal-error.component';
 
 describe('ErrorComponent', () => {
   let component: InternalErrorComponent;
   let fixture: ComponentFixture<InternalErrorComponent>;
 
+  const appConfigServiceMock = {
+    getAppConfig: () => ({
+      appInsightsKey: 'XXXXXXXX',
+      support: {
+        name: 'DARTS support',
+        emailAddress: 'support@darts',
+      },
+    }),
+  };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [InternalErrorComponent],
+      providers: [{ provide: AppConfigService, useValue: appConfigServiceMock }],
     });
     fixture = TestBed.createComponent(InternalErrorComponent);
     component = fixture.componentInstance;
