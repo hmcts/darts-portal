f48b670b3256312d756f05b940d469f6fec7b311
Bredan Pires <bredan.pires@hmcts.net>
2023-11-09 10:24:52 +0000

    unit tests

--- src/app/components/transcriptions/view-transcript/view-transcript.component.spec.ts
+++ src/app/components/transcriptions/view-transcript/view-transcript.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
 
 import { ViewTranscriptComponent } from './view-transcript.component';
 
@@ -6,10 +8,41 @@ describe('ViewTranscriptComponent', () => {
   let component: ViewTranscriptComponent;
   let fixture: ComponentFixture<ViewTranscriptComponent>;
 
+  const mockActivatedRoute = {
+    snapshot: {
+      data: {
+        userState: {
+          userId: 123,
+          userName: 'dev@local',
+          roles: [
+            {
+              roleId: 123,
+              roleName: 'local dev',
+              permissions: [
+                {
+                  permissionId: 1,
+                  permissionName: 'local dev permissions',
+                },
+              ],
+            },
+          ],
+        },
+      },
+      params: {
+        caseId: '1',
+        hearing_id: '1',
+        transcriptId: '1',
+      },
+      queryParams: { tab: 'Transcripts' },
+    },
+  };
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [ViewTranscriptComponent],
+      imports: [ViewTranscriptComponent, HttpClientModule],
+      providers: [{ provide: ActivatedRoute, useValue: mockActivatedRoute }],
     });
+
     fixture = TestBed.createComponent(ViewTranscriptComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
