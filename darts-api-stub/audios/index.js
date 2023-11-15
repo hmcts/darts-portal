495cc1a97778626cee98441d271cddf82fef466b
DavidTMann <d.mann@cgi.com>
2023-11-15 11:12:28 +0000

    DMP-1484 Preview audio player error message (#544)

--- darts-api-stub/audios/index.js
+++ darts-api-stub/audios/index.js
@@ -41,29 +41,33 @@ router.get('/hearings/:hearingId/audios', (req, res) => {
 });
 
 router.get('/preview/:mediaId', (req, res) => {
-  var filePath = __dirname + '/preview/preview.mp3';
-  var stat = fs.statSync(filePath);
-  var total = stat.size;
-  if (req.headers.range) {
-    var range = req.headers.range;
-    var parts = range.replace(/bytes=/, '').split('-');
-    var partialstart = parts[0];
-    var partialend = parts[1];
-
-    var start = parseInt(partialstart, 10);
-    var end = partialend ? parseInt(partialend, 10) : total - 1;
-    var chunksize = end - start + 1;
-    var readStream = fs.createReadStream(filePath, { start: start, end: end });
-    res.writeHead(206, {
-      'Content-Range': 'bytes ' + start + '-' + end + '/' + total,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': chunksize,
-      'Content-Type': 'audio/mpeg',
-    });
-    readStream.pipe(res);
+  if (req.params.mediaId === '2') {
+    res.sendStatus(500);
   } else {
-    res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'audio/mpeg' });
-    fs.createReadStream(filePath).pipe(res);
+    var filePath = __dirname + '/preview/preview.mp3';
+    var stat = fs.statSync(filePath);
+    var total = stat.size;
+    if (req.headers.range) {
+      var range = req.headers.range;
+      var parts = range.replace(/bytes=/, '').split('-');
+      var partialstart = parts[0];
+      var partialend = parts[1];
+
+      var start = parseInt(partialstart, 10);
+      var end = partialend ? parseInt(partialend, 10) : total - 1;
+      var chunksize = end - start + 1;
+      var readStream = fs.createReadStream(filePath, { start: start, end: end });
+      res.writeHead(206, {
+        'Content-Range': 'bytes ' + start + '-' + end + '/' + total,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunksize,
+        'Content-Type': 'audio/mpeg',
+      });
+      readStream.pipe(res);
+    } else {
+      res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'audio/mpeg' });
+      fs.createReadStream(filePath).pipe(res);
+    }
   }
 });
 
