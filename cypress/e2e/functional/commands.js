1e7bd94d3d3c81ef33f0c138eb00dd570ba1e793
Dean Bullock <dean.bullock@hmcts.net>
2023-11-23 17:11:16 +0000

    Add login as code to cypress tests

--- cypress/e2e/functional/commands.js
+++ cypress/e2e/functional/commands.js
@@ -1,15 +1,15 @@
 import 'cypress-axe';
-Cypress.Commands.add('login', (loginType = 'external') => {
+Cypress.Commands.add('login', (roleCode = 'admin', loginType = 'internal') => {
   cy.visit('/login');
-  if (loginType === 'external') {
-    cy.contains('I work with the HM Courts and Tribunals Service').click();
-  } else {
+  if (loginType === 'internal') {
     cy.contains("I'm an employee of HM Courts and Tribunals Service").click();
+  } else {
+    cy.contains('I work with the HM Courts and Tribunals Service').click();
   }
   cy.contains('Continue').click();
 
   cy.get('h1').should('contain', 'Stub login page');
-  cy.get('#login-admin').click();
+  cy.get(`#login-${roleCode}`).click();
   cy.get('.govuk-label-wrapper > .govuk-label').should('contain', 'Search for a case');
 });
 
