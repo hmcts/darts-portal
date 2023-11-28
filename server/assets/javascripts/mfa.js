86d8d0c76d8fa6f2cf2cf3644907d0c36379d972
DavidTMann <d.mann@cgi.com>
2023-11-28 16:48:34 +0000

    DMP-1455 MFA B2C Changes  (#596)

--- server/assets/javascripts/mfa.js
+++ server/assets/javascripts/mfa.js
@@ -1,6 +1,7 @@
 
 function insertBackLink(){
-    $(`<a href="${document.referrer}" class="govuk-back-link">Back</a>`).insertBefore("#attributeVerification");
+    $('button#cancel').insertBefore("#attributeVerification");
+    // $(`<a href="${document.referrer}" class="govuk-back-link">Back</a>`).insertBefore("#attributeVerification");
 }
 
 function insertInfoBox(){
@@ -29,6 +30,51 @@ function insertTitle(){
     $("<h1>Authenticator app</h1>").insertBefore("#attributeList");
 }
 
+function hideTryThisLink(){
+    $('a[href^="otpauth://"]').hide();
+}
+
+function showAccountDetails(){
+    hideTryThisLink();
+    const details = getAccountDetails();
+    const accountDetailsHtml = 
+    `<div class="account-details">
+        <p id="detailText">You can enter these account details into the authenticator app manually.</p>
+        
+        <label>Account Name</label>
+        <p id="accountId">${details.externalId}</p>
+
+        <label id="secretLabel">Secret</label>
+        <p id="secret">${details.secret}</p>
+    </div>`
+
+    $(accountDetailsHtml).insertAfter('#attributeList');
+}
+
+function getAccountDetails(){
+    const authDetails = $('#totpQrCodeControl-text').text();
+
+    const details = {};
+    const externalIdMatch = authDetails.match(/(?:[^:]+:){2}([^?]+)/);
+    const secretMatch = authDetails.match(/secret=([^&]+)/);
+
+    if (externalIdMatch && secretMatch) {
+        details.externalId = externalIdMatch[1];
+        details.secret = secretMatch[1];
+        console.log(details);
+        return details;
+    } else {
+        details.externalId = 'Error finding account details';
+        details.secret = 'Cannot find secret';
+        return details;
+    }
+}
+
+// when clicking "Can't scan? Try this", show Account Details
+$('#totpQrCodeControl-text').click(function () {
+    showAccountDetails();
+});
+
 function removeAppStores(){
     $('a#authenticatorAppIconControl-google').text('Google Play');
     $('a#authenticatorAppIconControl-apple').text('Apple App Store');
@@ -38,18 +84,14 @@ function removeText(){
     $('div.intro').remove();  
 }
 
-function removeButtons(){
-    $('button#cancel').remove();
-}
-
 function removeLinks(){
     $('a.helpLink').remove();
     $('li.authenticatorInfoControl_li').remove();
+    $('a[href^="otpauth://"]').attr('href', '#');
 }
 
 function removElements(){
     removeLinks();
-    removeButtons();
     removeText();
     removeAppStores();  
 }
