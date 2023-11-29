055d9433f2f7caaf6e3641b5e4dc9877f9a4c863
Bredan Pires <bredan.pires@hmcts.net>
2023-11-29 15:14:06 +0000

    Functional test for transcript requests table

--- cypress/e2e/functional/transcript-requests-spec.cy.js
+++ cypress/e2e/functional/transcript-requests-spec.cy.js
@@ -12,4 +12,12 @@ describe('Transcript requests', () => {
     cy.get('h1').should('contain', 'Transcript requests');
     cy.a11y();
   });
+
+  it('has Data in the table', () => {
+    cy.contains('Transcript requests').click();
+    cy.get('#transcript-requests-table').should('contain', 'Newcastle');
+    cy.get('#transcript-requests-table').should('contain', 'Court Log');
+    cy.get('#transcript-requests-table').should('contain', 'Manual');
+    cy.a11y();
+  })
 });
