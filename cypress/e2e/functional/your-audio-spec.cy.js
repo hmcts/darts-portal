0d617055b42529c7a41615d8d6854897e3fe91c1
Dean Bullock <dean.bullock@hmcts.net>
2023-11-29 10:32:43 +0000

    Fix nav roles and title case

--- cypress/e2e/functional/your-audio-spec.cy.js
+++ cypress/e2e/functional/your-audio-spec.cy.js
@@ -11,30 +11,30 @@ describe('Your audio', () => {
   });
 
   it('has title', () => {
-    cy.contains('Your Audio').click();
-    cy.get('h1').should('contain', 'Your Audio');
+    cy.contains('Your audio').click();
+    cy.get('h1').should('contain', 'Your audio');
   });
 
   it('has tabs', () => {
-    cy.contains('Your Audio').click();
+    cy.contains('Your audio').click();
     cy.get('.moj-sub-navigation__list').should('contain', 'Current');
     cy.get('.moj-sub-navigation__list').should('contain', 'Expired');
   });
 
   it('has "In progress" table', () => {
-    cy.contains('Your Audio').click();
+    cy.contains('Your audio').click();
     cy.get('#inProgressTable').should('contain', 'T20200190');
   });
 
   it('has "Ready" table', () => {
-    cy.contains('Your Audio').click();
+    cy.contains('Your audio').click();
     cy.get('#readyTable').should('contain', 'T20200333');
 
     cy.a11y();
   });
 
   it('has "Expired" table', () => {
-    cy.contains('Your Audio').click();
+    cy.contains('Your audio').click();
     cy.contains('Expired').click();
     cy.get('#expiredTable').should('contain', 'T20202110');
 
@@ -44,25 +44,25 @@ describe('Your audio', () => {
   it('should show correct Notification badge count', () => {
     //On landing page
     cy.get('#notifications').should('contain', '6');
-    cy.contains('Your Audio').click();
-    //On specific Your Audio page
+    cy.contains('Your audio').click();
+    //On specific Your audio page
     cy.get('#notifications').should('contain', '6');
   });
 
   it('should show correct number of Unread icons', () => {
-    cy.contains('Your Audio').click();
+    cy.contains('Your audio').click();
     cy.get('#readyTable').get('.unread').should('have.length', 6);
   });
 
   it('should reduce Notification count and unread icons when View is clicked', () => {
-    cy.contains('Your Audio').click();
+    cy.contains('Your audio').click();
     cy.contains('T20200331').parents('tr').contains('View').click();
 
     cy.get('#notifications').should('contain', '5');
   });
 
   it('View audio request and download', () => {
-    cy.contains('Your Audio').click();
+    cy.contains('Your audio').click();
     cy.contains('T20200331').parents('tr').contains('View').click();
     cy.contains('T20200331.zip').should('exist');
     cy.contains('Download audio file').click();
@@ -71,7 +71,7 @@ describe('Your audio', () => {
 
   it('View audio request and reject download due to permissions', () => {
     //T20200333 hardcoded in stub to return 403
-    cy.contains('Your Audio').click();
+    cy.contains('Your audio').click();
     cy.contains('T20200333').parents('tr').contains('View').click();
     cy.contains('T20200333.mp3').should('exist');
     cy.contains('Download audio file').click();
@@ -83,7 +83,7 @@ describe('Your audio', () => {
   });
 
   it('View audio request and delete', () => {
-    cy.contains('Your Audio').click();
+    cy.contains('Your audio').click();
     cy.contains('T20200334').parents('tr').contains('View').click();
     cy.contains('T20200334.mp3').should('exist');
     cy.contains('Delete audio file').click();
@@ -95,7 +95,7 @@ describe('Your audio', () => {
   });
 
   it('View audio request and playback audio', () => {
-    cy.contains('Your Audio').click();
+    cy.contains('Your audio').click();
     cy.contains('T20200192233').parents('tr').contains('View').click();
     cy.contains('T20200192233.mp3').should('exist');
 
@@ -118,7 +118,7 @@ describe('Your audio', () => {
   });
 
   it('should delete selected audio requests', () => {
-    cy.contains('Your Audio').click();
+    cy.contains('Your audio').click();
     cy.get('#readyTable tbody input[type="checkbox"]').first().click();
     cy.get('#delete-button').click();
     cy.get(navigationSelector).should('not.exist');
@@ -131,7 +131,7 @@ describe('Your audio', () => {
   });
 
   it('should clear failed audio requests', () => {
-    cy.contains('Your Audio').click();
+    cy.contains('Your audio').click();
     cy.contains('Clear').click();
     cy.get(navigationSelector).should('not.exist');
     cy.contains('Are you sure you want to delete this item');
@@ -141,7 +141,7 @@ describe('Your audio', () => {
   });
 
   it('link back to case via audio request', () => {
-    cy.contains('Your Audio').click();
+    cy.contains('Your audio').click();
     cy.contains('T20200190').click();
     cy.get('h1.govuk-heading-l').contains('CASE1001');
   });
@@ -170,7 +170,7 @@ describe('No audio requests', () => {
 
   it('shows no audio requests message and no notification badge', () => {
     cy.get('#notifications').should('not.exist');
-    cy.contains('Your Audio').click();
+    cy.contains('Your audio').click();
     cy.get('#notifications').should('not.exist');
     cy.get('p').should('contain', 'There are no audio files in progress or ready');
   });
