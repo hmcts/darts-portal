4fed0040bfd6d26ac6061914e8f16a8f4c022835
joshw-hmcts <josh.windus@hmcts.net>
2023-12-05 16:00:06 +0000

    Tests for approve-transcript-buttons

--- src/app/components/transcriptions/approve-transcript/approve-transcript-buttons/approve-transcript-buttons.component.spec.ts
+++ src/app/components/transcriptions/approve-transcript/approve-transcript-buttons/approve-transcript-buttons.component.spec.ts
@@ -1,14 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TranscriptionService } from '@services/transcription/transcription.service';
+import { of } from 'rxjs/internal/observable/of';
 
 import { ApproveTranscriptButtonsComponent } from './approve-transcript-buttons.component';
 
 describe('ApproveTranscriptButtonsComponent', () => {
   let component: ApproveTranscriptButtonsComponent;
   let fixture: ComponentFixture<ApproveTranscriptButtonsComponent>;
+  const fakeTranscriptionService = {
+    approveTranscriptionRequest: jest.fn().mockReturnValue(of({})),
+    rejectTranscriptionRequest: jest.fn().mockReturnValue(of({})),
+  } as unknown as TranscriptionService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ApproveTranscriptButtonsComponent],
+      imports: [ApproveTranscriptButtonsComponent, HttpClientTestingModule, RouterTestingModule],
+      providers: [{ provide: TranscriptionService, useValue: fakeTranscriptionService }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ApproveTranscriptButtonsComponent);
@@ -18,5 +27,30 @@ describe('ApproveTranscriptButtonsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render reject-reason box if no selected', () => {
+    const compiled = fixture.nativeElement;
+    component.approveFormControl.setValue('No');
+    fixture.detectChanges();
+    const rejectReason = compiled.querySelector('#reject-reason');
+    expect(rejectReason).toBeTruthy();
+  });
+
+  it('should call rejectTranscriptionRequest from transcriptionService when reason populated', () => {
+    const request = { id: 1, reason: "I don't like this transcription" };
+    component.transcriptId = request.id;
+    component.approveFormControl.setValue('No');
+    component.rejectReasonFormControl.setValue(request.reason);
+    component.onSubmit();
+    expect(fakeTranscriptionService.rejectTranscriptionRequest).toHaveBeenCalledWith(request.id, request.reason);
+  });
+
+  it('should call approveTranscriptionRequest from transcriptionService', () => {
+    const request = { id: 1 };
+    component.approveFormControl.setValue('Yes');
+    component.transcriptId = request.id;
+    component.onSubmit();
+    expect(fakeTranscriptionService.approveTranscriptionRequest).toHaveBeenCalledWith(request.id);
   });
 });
