7b3b988d7b11f8c9f2166653b2b955efb40f8e43
Dean Bullock <dean.bullock@hmcts.net>
2023-12-08 14:12:14 +0000

    Remove any type

--- src/app/validators/max-file-size.validator.ts
+++ src/app/validators/max-file-size.validator.ts
@@ -1,7 +1,7 @@
 import { AbstractControl, ValidatorFn } from '@angular/forms';
 
 export function maxFileSizeValidator(maxSizeInMB: number): ValidatorFn {
-  return (control: AbstractControl): { [key: string]: any } | null => {
+  return (control: AbstractControl): { [key: string]: unknown } | null => {
     const fileSizeInBytes = (control.value as File)?.size;
     const maxSizeInBytes = maxSizeInMB * 1024 * 1024;
 
