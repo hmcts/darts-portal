0e31728b65d05c4eb584cce5c6b8ee5f6f404084
David Mann <david.mann1@hmcts.net>
2023-12-13 13:06:32 +0000

    updated js code to hide input errors on login load

--- server/assets/javascripts/login.js
+++ server/assets/javascripts/login.js
@@ -10,6 +10,9 @@ function loginAccessibility(){
   }
 }
 
+$(document).on('click', hideRequiredErrorsOnLoad);
+$(document).on('keydown', hideRequiredErrorsOnLoad);
+
 function displayErrors() {
   removeErrors();
   createErrorSummaryBox('login');
