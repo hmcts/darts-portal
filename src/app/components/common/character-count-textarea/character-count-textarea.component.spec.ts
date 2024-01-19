a83f4f6e9b918cb611efce513a0b44c83a05e2e6
Bredan Pires <bredan.pires@hmcts.net>
2024-01-19 12:07:20 +0000

    more unit tests

--- src/app/components/common/character-count-textarea/character-count-textarea.component.spec.ts
+++ src/app/components/common/character-count-textarea/character-count-textarea.component.spec.ts
@@ -37,4 +37,10 @@ describe('CharacterCountTextareaComponent', () => {
     const result = component.remainingCharacterCount;
     expect(result).toEqual(195);
   });
+
+  it('should get the remaining Character Count for an empty form control', () => {
+    component.maxCharacterLimit = 200;
+    const result = component.remainingCharacterCount;
+    expect(result).toEqual(200);
+  });
 });
