374a5f6cb1ce72cee9159cb8a3120a501583b841
Dean Bullock <dean.bullock@hmcts.net>
2024-01-23 09:20:34 +0000

    Add pipe tests

--- src/app/pipes/array-display.pipe.spec.ts
+++ src/app/pipes/array-display.pipe.spec.ts
@@ -1,8 +1,37 @@
 import { ArrayDisplayPipe } from './array-display.pipe';
 
 describe('ArrayDisplayPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ArrayDisplayPipe();
+  let pipe: ArrayDisplayPipe;
+
+  beforeEach(() => {
+    pipe = new ArrayDisplayPipe();
+  });
+
+  it('should create an instance', () => {
     expect(pipe).toBeTruthy();
+  });
+
+  it('should return an empty string when the array is null', () => {
+    const arr: string[] = [];
+    const result = pipe.transform(arr);
+    expect(result).toEqual('');
+  });
+
+  it('should return an empty string when the array is empty', () => {
+    const arr: string[] = [];
+    const result = pipe.transform(arr);
+    expect(result).toEqual('');
+  });
+
+  it('should return the first element when the array has only one element', () => {
+    const arr = ['Single'];
+    const result = pipe.transform(arr);
+    expect(result).toEqual('Single');
+  });
+
+  it('should return "Multiple" when the array has more than one element', () => {
+    const arr = ['Element 1', 'Element 2'];
+    const result = pipe.transform(arr);
+    expect(result).toEqual('Multiple');
   });
 });
