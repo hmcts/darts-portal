1561dfdca5ec952855c4636b1e7e08cf8f104400
Bredan Pires <bredan.pires@hmcts.net>
2024-01-30 16:17:46 +0000

    fix logic

--- server/middleware/is-authenticated.ts
+++ server/middleware/is-authenticated.ts
@@ -2,12 +2,12 @@ import { NextFunction, Request, Response } from 'express';
 import { AuthenticationUtils } from '../utils/authentication-utils';
 
 export default (req: Request, res: Response, next: NextFunction): void => {
-  if (AuthenticationUtils.isJwtExpired(req.session?.securityToken?.accessToken)) {
+  if (
+    AuthenticationUtils.isJwtExpired(req.session?.securityToken?.accessToken) ||
+    !req.session.securityToken?.userState?.userId
+  ) {
     res.sendStatus(401);
   } else {
     next();
-  }
-  if (!req.session.securityToken?.userState?.userId) {
-    res.sendStatus(401);
   }
 };
