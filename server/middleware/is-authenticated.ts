9ade1620e021d9efb90d36c18f4e75a37ec190c1
bpires2 <134072550+bpires2@users.noreply.github.com>
2024-01-31 14:59:23 +0000

    DMP-2048 (#782)

--- server/middleware/is-authenticated.ts
+++ server/middleware/is-authenticated.ts
@@ -1,8 +1,11 @@
-import { Request, Response, NextFunction } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { AuthenticationUtils } from '../utils/authentication-utils';
 
 export default (req: Request, res: Response, next: NextFunction): void => {
-  if (AuthenticationUtils.isJwtExpired(req.session?.securityToken?.accessToken)) {
+  if (
+    AuthenticationUtils.isJwtExpired(req.session?.securityToken?.accessToken) ||
+    !req.session.securityToken?.userState?.userId
+  ) {
     res.sendStatus(401);
   } else {
     next();
