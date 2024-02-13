27899d0e2ce10a10d1a413146f6af31274b9158b
joshw-hmcts <josh.windus@hmcts.net>
2024-02-13 17:04:54 +0000

    Added mapper for annotations and appropriate tests

--- src/app/portal/services/mapping/mapping.service.spec.ts
+++ src/app/portal/services/mapping/mapping.service.spec.ts
@@ -1,6 +1,9 @@
+import { DateTime } from 'luxon';
 import { TestBed } from '@angular/core/testing';
 
 import { MappingService } from './mapping.service';
+import { Transcript } from '@portal-types/transcriptions/transcript.type';
+import { AnnotationsData } from '@portal-types/annotations/annotations-data.interface';
 
 describe('MappingService', () => {
   let service: MappingService;
@@ -12,5 +15,79 @@ describe('MappingService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('#mapTranscriptRequestToRows', () => {
+    it('should map correctly', () => {
+      const hearingDateDateTime = DateTime.fromISO('2024-02-01');
+      const requestedOnDateTime = DateTime.fromISO('2024-01-01');
+      const transcripts: Transcript[] = [
+        {
+          id: 1,
+          hearingId: 1,
+          hearingDate: hearingDateDateTime,
+          type: 'TYPE',
+          requestedOn: requestedOnDateTime,
+          requestedByName: 'Mrs Test',
+          status: 'Complete',
+        },
+      ];
+      const result = service.mapTranscriptRequestToRows(transcripts);
+      expect(result).toStrictEqual([
+        {
+          hearingDate: hearingDateDateTime,
+          id: 1,
+          requestedBy: 'Mrs Test',
+          requestedOn: requestedOnDateTime,
+          status: 'Complete',
+          type: 'TYPE',
+        },
+      ]);
+    });
+  });
+
+  describe('#mapAnnotationsDataToAnnotations', () => {
+    it('should map correctly', () => {
+      const hearingDateIso = '2024-02-01';
+      const annotationTimeStampIso = '2024-01-01';
+      const uploadedTimeStampIso = '2024-03-01';
+      const transcripts: AnnotationsData[] = [
+        {
+          annotation_id: 1,
+          hearing_id: 2,
+          hearing_date: hearingDateIso,
+          annotation_ts: annotationTimeStampIso,
+          annotation_text: 'TEST',
+          annotation_documents: [
+            {
+              annotation_document_id: 3,
+              file_name: 'filename.docx',
+              file_type: 'DOC',
+              uploaded_by: 'Dr Test',
+              uploaded_ts: uploadedTimeStampIso,
+            },
+          ],
+        },
+      ];
+      const result = service.mapAnnotationsDataToAnnotations(transcripts);
+      expect(result).toStrictEqual([
+        {
+          annotationId: 1,
+          hearingId: 2,
+          hearingDate: DateTime.fromISO(hearingDateIso),
+          annotationTs: DateTime.fromISO(annotationTimeStampIso),
+          annotationText: 'TEST',
+          annotationDocuments: [
+            {
+              annotationDocumentId: 3,
+              fileName: 'filename.docx',
+              fileType: 'DOC',
+              uploadedBy: 'Dr Test',
+              uploadedTs: DateTime.fromISO(uploadedTimeStampIso),
+            },
+          ],
+        },
+      ]);
+    });
   });
 });
--- src/app/portal/services/mapping/mapping.service.ts
+++ src/app/portal/services/mapping/mapping.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from '@angular/core';
+import { Annotations } from '@portal-types/annotations/annotations.type';
+import { AnnotationsData } from '@portal-types/annotations/annotations-data.interface';
+import { AnnotationDocumentData } from '@portal-types/annotations/annotations-document-data.interface';
 import { Transcript } from '@portal-types/transcriptions/transcript.type';
 import { TranscriptsRow } from '@portal-types/transcriptions/transcripts-row.type';
+import { DateTime } from 'luxon';
 
 @Injectable({
   providedIn: 'root',
@@ -17,5 +21,22 @@ export class MappingService {
         status: transcript.status,
       };
     });
+  }
+
+  mapAnnotationsDataToAnnotations(annotationsData: AnnotationsData[]): Annotations[] {
+    return annotationsData.map((a) => ({
+      annotationId: a.annotation_id,
+      hearingId: a.hearing_id,
+      hearingDate: DateTime.fromISO(a.hearing_date),
+      annotationTs: DateTime.fromISO(a.annotation_ts),
+      annotationText: a.annotation_text,
+      annotationDocuments: a.annotation_documents.map((ad: AnnotationDocumentData) => ({
+        annotationDocumentId: ad.annotation_document_id,
+        fileName: ad.file_name,
+        fileType: ad.file_type,
+        uploadedBy: ad.uploaded_by,
+        uploadedTs: DateTime.fromISO(ad.uploaded_ts),
+      })),
+    }));
   }
 }
