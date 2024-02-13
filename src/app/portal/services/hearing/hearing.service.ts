b3adb3b3823f9149a6220355f4943a66b0b5f336
joshw-hmcts <josh.windus@hmcts.net>
2024-02-13 17:03:04 +0000

    Updated hearing service to get specific annotations

--- src/app/portal/services/hearing/hearing.service.ts
+++ src/app/portal/services/hearing/hearing.service.ts
@@ -1,7 +1,16 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { HearingAudio, HearingEvent, PostAudioRequest, PostAudioResponse } from '@portal-types/index';
-import { Observable, catchError, of, throwError } from 'rxjs';
+import { Injectable, Injector, inject } from '@angular/core';
+
+import { Annotations } from '@portal-types/annotations/annotations.type';
+import { AnnotationsData } from '@portal-types/annotations/annotations-data.interface';
+import { HearingAudio } from '@portal-types/hearing/hearing-audio.interface';
+import { HearingEvent } from '@portal-types/hearing/hearing-event.interface';
+import { PostAudioRequest } from '@portal-types/hearing/post-audio-request.interface';
+import { PostAudioResponse } from '@portal-types/hearing/post-audio-response.interface';
+
+import { GET_HEARINGS_PATH } from '@services/case/case.service';
+import { MappingService } from '@services/mapping/mapping.service';
+import { Observable, catchError, of, throwError, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,7 +19,7 @@ export class HearingService {
   constructor(private readonly http: HttpClient) {}
 
   getEvents(hearingId: string | number): Observable<HearingEvent[]> {
-    return this.http.get<HearingEvent[]>(`api/hearings/${hearingId}/events`).pipe(
+    return this.http.get<HearingEvent[]>(`${GET_HEARINGS_PATH}/${hearingId}/events`).pipe(
       catchError((error) => {
         if (error.status === 404) return of([]);
         return throwError(() => error);
@@ -29,5 +38,12 @@ export class HearingService {
 
   requestAudio(audioRequest: PostAudioRequest): Observable<PostAudioResponse> {
     return this.http.post<PostAudioResponse>('api/audio-requests', audioRequest);
+  }
+
+  getAnnotations(caseId: number): Observable<Annotations[]> {
+    const mappingService = inject(MappingService);
+    return this.http
+      .get<AnnotationsData[]>(`${GET_HEARINGS_PATH}/${caseId}/annotations`)
+      .pipe(map(mappingService.mapAnnotationsDataToAnnotations));
   }
 }
