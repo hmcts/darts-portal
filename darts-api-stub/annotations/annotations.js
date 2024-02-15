83933a4ccb6980fd3250a36c4dd2ad57e99c05ba
Bredan Pires <bredan.pires@hmcts.net>
2024-02-15 11:46:15 +0000

    yarn lint

--- darts-api-stub/annotations/annotations.js
+++ darts-api-stub/annotations/annotations.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require('path');
 
 router.get('/:annotationId/documents/:annotationDocumentId', (req, res) => {
-    res.sendFile(path.join(__dirname, './download', 'TestDoc.docx'));
+  res.sendFile(path.join(__dirname, './download', 'TestDoc.docx'));
 });
 
 module.exports = router;
