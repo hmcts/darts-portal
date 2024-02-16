ad499fdf25f795b15adbdd823d4a2d1f4ea349c9
joshw-hmcts <josh.windus@hmcts.net>
2024-02-16 17:18:59 +0000

    User can now delete annotations from Hearing

--- darts-api-stub/annotations/annotation.js
+++ darts-api-stub/annotations/annotation.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { localArray } = require('../localArray');
 const path = require('path');
+const { userIdhasAnyRoles } = require('../users');
+const { ADMIN, JUDGE } = require('../roles');
 
 const router = express.Router();
 
@@ -19,6 +21,16 @@ const getAnnotationDocumentByAnnotationAndDocumentId = (annotationId, annotation
   );
 };
 
+const deleteAnnotationById = (annotationId) => {
+  const annotations = localArray('annotations');
+  const annotation = annotations.value.find((annotation) => annotation.annotation_id === parseInt(annotationId));
+  if (!annotation) return null;
+  else annotations.value.splice(annotations.value.indexOf(annotation), 1);
+  // A stupid workaround that works
+  annotations.value = annotations.value;
+  return true;
+};
+
 // annotations stub data
 router.get('/:annotationId/documents/:annotationDocumentId', (req, res) => {
   const document = getAnnotationDocumentByAnnotationAndDocumentId(
@@ -32,6 +44,25 @@ router.get('/:annotationId/documents/:annotationDocumentId', (req, res) => {
   const fileName = document.file_name;
   res.set('Content-Disposition', `attachment; filename="${fileName}"`);
   res.sendFile(path.join(__dirname, './docs', 'annotation.docx'));
+});
+
+router.delete('/:annotationId', (req, res) => {
+  const { annotationId } = req.params;
+  if (!userIdhasAnyRoles([ADMIN, JUDGE], req.headers.user_id)) {
+    res.status(403).send({
+      detail: `You do not have permission to delete annotation with annotationId ${req.params?.annotationId}'.`,
+    });
+    return;
+  }
+  const annotationDeleted = deleteAnnotationById(annotationId);
+  if (!annotationDeleted) {
+    res.status(404).send({
+      detail: `Annotation with annotationId '${annotationId}' does not exist'.`,
+    });
+    return;
+  }
+  // Otherwise return 204
+  res.status(204).send();
 });
 
 module.exports = router;
