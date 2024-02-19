b758386f6109b0d2e0bd3fe8f567f62e7dab708b
Bredan Pires <bredan.pires@hmcts.net>
2024-02-19 17:18:11 +0000

    working breadcrumbs

--- src/app/portal/components/hearing/add-annotation/add-annotation.component.ts
+++ src/app/portal/components/hearing/add-annotation/add-annotation.component.ts
@@ -1,22 +1,32 @@
-import { Component, Input, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { BreadcrumbComponent } from '@common/breadcrumb/breadcrumb.component';
 import { BreadcrumbDirective } from '@directives/breadcrumb.directive';
+import { LuxonDatePipe } from '@pipes/luxon-date.pipe';
 import { HeaderService } from '@services/header/header.service';
 import { DateTime } from 'luxon';
 
 @Component({
   selector: 'app-add-annotation',
   standalone: true,
-  imports: [BreadcrumbComponent, BreadcrumbDirective],
+  imports: [BreadcrumbComponent, BreadcrumbDirective, LuxonDatePipe],
   templateUrl: './add-annotation.component.html',
   styleUrl: './add-annotation.component.scss',
 })
 export class AddAnnotationComponent implements OnInit {
-  @Input() caseId!: string;
-  @Input() caseNumber!: number;
-  @Input() hearingDate!: DateTime;
+  caseId!: string;
+  caseNumber!: number;
+  hearingDate!: DateTime;
 
   headerService = inject(HeaderService);
+  router = inject(Router);
+
+  constructor() {
+    const state = this.router.getCurrentNavigation()?.extras.state;
+    this.caseId = state!.caseId;
+    this.caseNumber = state!.caseNumber;
+    this.hearingDate = state!.hearingDate;
+  }
 
   ngOnInit(): void {
     setTimeout(() => this.headerService.hideNavigation(), 0);
