44f7c3a30f7f982851150d4ffd7e6912a4a7e9e1
Dean Bullock <dean.bullock@hmcts.net>
2024-02-20 11:11:08 +0000

    Remove old preview stubbed endpoint

--- darts-api-stub/audios/index.js
+++ darts-api-stub/audios/index.js
@@ -55,38 +55,6 @@ router.get('/hearings/:hearingId/audios', (req, res) => {
   }
 });
 
-// To be removed
-router.get('/preview-old/:mediaId', (req, res) => {
-  if (req.params.mediaId === '4') res.sendStatus(403);
-  if (req.params.mediaId === '5') res.sendStatus(404);
-  if (req.params.mediaId === '6') res.sendStatus(500);
-
-  var filePath = __dirname + '/preview/preview.mp3';
-  var stat = fs.statSync(filePath);
-  var total = stat.size;
-  if (req.headers.range) {
-    var range = req.headers.range;
-    var parts = range.replace(/bytes=/, '').split('-');
-    var partialstart = parts[0];
-    var partialend = parts[1];
-
-    var start = parseInt(partialstart, 10);
-    var end = partialend ? parseInt(partialend, 10) : total - 1;
-    var chunksize = end - start + 1;
-    var readStream = fs.createReadStream(filePath, { start: start, end: end });
-    res.writeHead(206, {
-      'Content-Range': 'bytes ' + start + '-' + end + '/' + total,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': chunksize,
-      'Content-Type': 'audio/mpeg',
-    });
-    readStream.pipe(res);
-  } else {
-    res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'audio/mpeg' });
-    fs.createReadStream(filePath).pipe(res);
-  }
-});
-
 // SSE Preview Audio Stub
 router.get('/preview/:mediaId', (req, res) => {
   if (req.params.mediaId === '4') {
