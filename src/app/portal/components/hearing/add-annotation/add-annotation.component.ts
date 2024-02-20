5ab84ea9c29a2847724fec9cc1f71eb0c489d87b
Bredan Pires <bredan.pires@hmcts.net>
2024-02-20 14:17:59 +0000

    html elements on page

--- src/app/portal/components/hearing/add-annotation/add-annotation.component.ts
+++ src/app/portal/components/hearing/add-annotation/add-annotation.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit, inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { BreadcrumbComponent } from '@common/breadcrumb/breadcrumb.component';
+import { FileUploadComponent } from '@common/file-upload/file-upload.component';
+import { GovukHeadingComponent } from '@common/govuk-heading/govuk-heading.component';
+import { GovukTextareaComponent } from '@common/govuk-textarea/govuk-textarea.component';
 import { BreadcrumbDirective } from '@directives/breadcrumb.directive';
 import { LuxonDatePipe } from '@pipes/luxon-date.pipe';
 import { HeaderService } from '@services/header/header.service';
+import { maxFileSizeValidator } from '@validators/max-file-size.validator';
 import { DateTime } from 'luxon';
 
 @Component({
   selector: 'app-add-annotation',
   standalone: true,
-  imports: [BreadcrumbComponent, BreadcrumbDirective, LuxonDatePipe],
+  imports: [
+    BreadcrumbComponent,
+    BreadcrumbDirective,
+    LuxonDatePipe,
+    GovukHeadingComponent,
+    FileUploadComponent,
+    ReactiveFormsModule,
+    GovukTextareaComponent,
+  ],
   templateUrl: './add-annotation.component.html',
   styleUrl: './add-annotation.component.scss',
 })
@@ -20,6 +33,9 @@ export class AddAnnotationComponent implements OnInit {
 
   headerService = inject(HeaderService);
   router = inject(Router);
+
+  fileControl = new FormControl<File | null>(null, [Validators.required, maxFileSizeValidator(10)]);
+  annotationComments = new FormControl('');
 
   constructor() {
     const state = this.router.getCurrentNavigation()?.extras.state;
