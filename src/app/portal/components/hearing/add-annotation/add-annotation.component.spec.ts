b83c199026f6f53c0197db35ba1f2475bae3c284
Bredan Pires <bredan.pires@hmcts.net>
2024-02-22 14:08:15 +0000

    unit tests for add annotation

--- src/app/portal/components/hearing/add-annotation/add-annotation.component.spec.ts
+++ src/app/portal/components/hearing/add-annotation/add-annotation.component.spec.ts
@@ -1,15 +1,50 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { DatePipe } from '@angular/common';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ActivatedRoute, Navigation, Router } from '@angular/router';
+import { LuxonDatePipe } from '@pipes/luxon-date.pipe';
+import { AnnotationService } from '@services/annotation/annotation.service';
+import { DateTime } from 'luxon';
 import { AddAnnotationComponent } from './add-annotation.component';
 
 describe('AddAnnotationComponent', () => {
   let component: AddAnnotationComponent;
   let fixture: ComponentFixture<AddAnnotationComponent>;
+  let router: Router;
+
+  const mockActivatedRoute = {
+    snapshot: {
+      params: {
+        requestId: 12378,
+      },
+    },
+  };
+
+  const mockNavigationExtras = {
+    extras: {
+      state: {
+        caseId: '1',
+        caseNumber: 'C20220620001',
+        hearingDate: DateTime.fromISO('2023-10-11T00:00:00.000Z'),
+        hearingId: 3,
+      },
+    },
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AddAnnotationComponent],
+      imports: [AddAnnotationComponent, HttpClientTestingModule],
+      providers: [
+        AnnotationService,
+        LuxonDatePipe,
+        DatePipe,
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+      ],
     }).compileComponents();
+
+    router = TestBed.inject(Router);
+    jest.spyOn(router, 'getCurrentNavigation').mockReturnValue(mockNavigationExtras as unknown as Navigation);
 
     fixture = TestBed.createComponent(AddAnnotationComponent);
     component = fixture.componentInstance;
