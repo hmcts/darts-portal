e97d74d9f535f45ed0596a71c3cacfacf2d90697
Bredan Pires <bredan.pires@hmcts.net>
2024-02-23 12:56:54 +0000

    fix unit tests

--- src/app/portal/components/hearing/add-annotation/add-annotation-success/add-annotation-success.component.spec.ts
+++ src/app/portal/components/hearing/add-annotation/add-annotation-success/add-annotation-success.component.spec.ts
@@ -1,14 +1,24 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { ActivatedRoute } from '@angular/router';
 import { AddAnnotationSuccessComponent } from './add-annotation-success.component';
 
 describe('AddAnnotationSuccessComponent', () => {
   let component: AddAnnotationSuccessComponent;
   let fixture: ComponentFixture<AddAnnotationSuccessComponent>;
 
+  const mockActivatedRoute = {
+    snapshot: {
+      params: {
+        userState: { userId: 123 },
+      },
+    },
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AddAnnotationSuccessComponent],
+      providers: [{ provide: ActivatedRoute, useValue: mockActivatedRoute }],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AddAnnotationSuccessComponent);
