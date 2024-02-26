297a33ebf8de812b04aedcba0fc78bacbc55c4a6
bpires2 <134072550+bpires2@users.noreply.github.com>
2024-02-26 16:10:09 +0000

    DMP-1623-Annotation-Confirmation-Screen (#851)

--- src/app/portal/components/hearing/add-annotation/add-annotation-success/add-annotation-success.component.ts
+++ src/app/portal/components/hearing/add-annotation/add-annotation-success/add-annotation-success.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-add-annotation-success',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './add-annotation-success.component.html',
   styleUrl: './add-annotation-success.component.scss',
 })
