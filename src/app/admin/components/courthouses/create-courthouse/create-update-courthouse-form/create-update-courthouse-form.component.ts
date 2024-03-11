be70cae1b0746a134f33f659947a0cf8d229b8f7
joshw-hmcts <josh.windus@hmcts.net>
2024-03-11 11:17:57 +0000

    Swapped to if (!this.form.valid)

--- src/app/admin/components/courthouses/create-courthouse/create-update-courthouse-form/create-update-courthouse-form.component.ts
+++ src/app/admin/components/courthouses/create-courthouse/create-update-courthouse-form/create-update-courthouse-form.component.ts
@@ -86,7 +86,7 @@ export class CreateUpdateCourthouseFormComponent implements OnInit {
   onSubmit() {
     this.form.markAllAsTouched();
 
-    if (this.form.invalid) {
+    if (!this.form.valid) {
       this.form.statusChanges.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(() => {
         this.errors.emit(this.formService.getErrorSummary(this.form, controlErrors));
       });
