ed47392a0de0ada6c06be13e993045dbdb024a64
joshw-hmcts <josh.windus@hmcts.net>
2024-03-12 12:33:53 +0000

    Show banner on creation of new courthouse

--- src/app/admin/components/courthouses/courthouse-record/courthouse-record.component.ts
+++ src/app/admin/components/courthouses/courthouse-record/courthouse-record.component.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { DetailsTableComponent } from '@common/details-table/details-table.component';
+import { GovukBannerComponent } from '@common/govuk-banner/govuk-banner.component';
 import { LoadingComponent } from '@common/loading/loading.component';
 import { TabsComponent } from '@common/tabs/tabs.component';
 import { NotFoundComponent } from '@components/error/not-found/not-found.component';
 import { TabDirective } from '@directives/tab.directive';
 import { LuxonDatePipe } from '@pipes/luxon-date.pipe';
 import { CourthouseService } from '@services/courthouses/courthouses.service';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-courthouse',
@@ -23,6 +25,7 @@ import { CourthouseService } from '@services/courthouses/courthouses.service';
     DetailsTableComponent,
     NotFoundComponent,
     LoadingComponent,
+    GovukBannerComponent,
   ],
 })
 export class CourthouseRecordComponent {
@@ -33,6 +36,7 @@ export class CourthouseRecordComponent {
   courthouse$ = this.courthouseService.getCourthouseWithRegionsAndSecurityGroups(
     this.route.snapshot.params.courthouseId
   );
+  isNewCourthouse$ = this.route.queryParams.pipe(map((params) => !!params.newCourthouse));
 
   formatSecurityGroupLinks(securityGroups: SecurityGroup[] | undefined) {
     return securityGroups?.map((securityGroup) => {
