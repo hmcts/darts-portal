7eb9429d943ad90dc0ec2d9aff3a4c0dd17da084
Dean Bullock <dean.bullock@hmcts.net>
2024-04-08 11:19:09 +0100

    Add trim to uniqueness validator

--- src/app/core/validators/retention-policy.validator.ts
+++ src/app/core/validators/retention-policy.validator.ts
@@ -3,7 +3,7 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 export const uniqueValidator = (existingPolicies: RetentionPolicy[], key: keyof RetentionPolicy): ValidatorFn => {
   return (control: AbstractControl): ValidationErrors | null => {
-    const value = control.value;
+    const value = control.value.trim();
 
     if (!existingPolicies?.find((policy) => policy[key] == value)) {
       return null;
