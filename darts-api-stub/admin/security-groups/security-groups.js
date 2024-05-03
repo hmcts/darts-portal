425a05abea2e676f4d228956a0b2b4283fbcb61b
Bredan Pires <bredan.pires@hmcts.net>
2024-05-03 17:16:08 +0100

    Functional test fix

--- darts-api-stub/admin/security-groups/security-groups.js
+++ darts-api-stub/admin/security-groups/security-groups.js
@@ -1,4 +1,7 @@
 const express = require('express');
+
+const { localArray } = require('../../localArray');
+
 const router = express.Router();
 
 const defaultSecurityGroups = [
@@ -147,9 +150,13 @@ const defaultSecurityGroups = [
   },
 ];
 
+const securityGroups = localArray('securityGroups');
+// Clear out old values on restart
+securityGroups.value = defaultSecurityGroups;
+
 router.patch('/:id', (req, res) => {
   const id = req.params.id;
-  const securityGroup = securityGroups.find((securityGroup) => securityGroup.id == id);
+  const securityGroup = securityGroups.value.find((securityGroup) => securityGroup.id == id);
   if (!securityGroup) return res.status(404).send('Security group not found');
   Object.assign(securityGroup, req.body);
   res.send(securityGroup);
@@ -157,7 +164,7 @@ router.patch('/:id', (req, res) => {
 
 router.get('/:id', (req, res) => {
   const id = req.params.id;
-  const securityGroup = securityGroups.find((securityGroup) => securityGroup.id == id);
+  const securityGroup = securityGroups.value.find((securityGroup) => securityGroup.id == id);
   if (!securityGroup) return res.status(404).send('Security group not found');
   res.send(securityGroup);
 });
