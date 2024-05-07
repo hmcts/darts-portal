b406533ceb2627a7828f1f6c52ec7e02d68e0244
Dave Thompson <davethompson21@gmail.com>
2024-05-07 11:30:48 +0100

    TS path fix

--- server/stubs/security-roles.ts
+++ server/stubs/security-roles.ts
@@ -1,5 +1,5 @@
 import { StubResponse } from '.';
-import data from '@stubs/securityRoles.json';
+import data from '../../darts-api-stub/data/securityRoles.json';
 
 export const securityRoles: StubResponse[] = [
   {
