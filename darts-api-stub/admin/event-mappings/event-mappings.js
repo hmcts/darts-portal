2f588044b1615849103d9e5ee934ea1ecfdcd13d
David Mann <david.mann1@hmcts.net>
2024-05-15 15:47:25 +0100

    stub fix

--- darts-api-stub/admin/event-mappings/event-mappings.js
+++ darts-api-stub/admin/event-mappings/event-mappings.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const { localArray } = require('../../localArray');
 const eventMapping = localArray('eventMappings');
 
-router.get('/', (_, res) => res.json(eventMapping));
+router.get('/', (_, res) => res.json(eventMapping.value));
 
 module.exports = router;
