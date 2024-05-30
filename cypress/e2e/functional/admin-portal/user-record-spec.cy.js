39da8063b186863ff9c9314911c6fb719416694e
David Mann <david.mann1@hmcts.net>
2024-05-30 11:31:22 +0100

    ft fixes

--- cypress/e2e/functional/admin-portal/user-record-spec.cy.js
+++ cypress/e2e/functional/admin-portal/user-record-spec.cy.js
@@ -336,13 +336,12 @@ describe('Admin - User record screen', () => {
       cy.get('#transcriptRequestsTable tbody tr')
         .eq(0)
         .within(() => {
-          cy.get('td').eq(0).should('contain.text', '1');
-          cy.get('td').eq(1).should('contain.text', 'C0000000006');
+          cy.get('td').eq(0).should('contain.text', '7');
+          cy.get('td').eq(1).should('contain.text', 'C0000000007');
           cy.get('td').eq(2).should('contain.text', 'Southampton');
           cy.get('td').eq(3).should('contain.text', '06 Jan 2022');
-          cy.get('td').eq(4).should('contain.text', '30 Dec 2023 08:32');
           cy.get('td').eq(5).should('contain.text', 'With Transcriber');
-          cy.get('td').eq(6).should('contain.text', 'Automatic');
+          cy.get('td').eq(6).should('contain.text', 'Manual');
         });
 
       cy.a11y();
@@ -354,9 +353,8 @@ describe('Admin - User record screen', () => {
 
       cy.get('#transcripts-tab').click();
 
-      cy.get('#transcripts-tab .count').should('contain', '7');
-
       cy.get('#showAll').check();
+      cy.get('#transcripts-tab .count').should('contain', '7');
 
       cy.get('#transcriptRequestsTable tbody tr').should('have.length', 7);
       cy.get('#transcriptRequestsTable thead tr').within(() => {
