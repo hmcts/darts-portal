579e09ed79536ad16313b86b91c88905eb9bad18
David Mann <david.mann1@hmcts.net>
2024-05-31 14:17:39 +0100

    uncomment add event mapping tests, oops

--- cypress/e2e/functional/admin-portal/event-mappings-spec.cy.js
+++ cypress/e2e/functional/admin-portal/event-mappings-spec.cy.js
@@ -8,149 +8,149 @@ describe('Admin - Event mappings screen', () => {
     cy.injectAxe();
   });
 
-  // it('page elements', () => {
-  //   cy.get('app-govuk-heading h1').contains('System configuration');
-  //   cy.get('app-govuk-heading h2').contains('Event mappings');
-  //   cy.get('.govuk-button--secondary').contains('Add event mapping');
+  it('page elements', () => {
+    cy.get('app-govuk-heading h1').contains('System configuration');
+    cy.get('app-govuk-heading h2').contains('Event mappings');
+    cy.get('.govuk-button--secondary').contains('Add event mapping');
 
-  //   cy.get('app-data-table').contains('Event map 1');
-  //   cy.get('app-data-table').contains('Mapping entry 3');
-  //   cy.get('app-data-table').contains('Fourth event mapping');
-  //   cy.get('app-data-table').contains('Prosecution opened');
+    cy.get('app-data-table').contains('Event map 1');
+    cy.get('app-data-table').contains('Mapping entry 3');
+    cy.get('app-data-table').contains('Fourth event mapping');
+    cy.get('app-data-table').contains('Prosecution opened');
 
-  //   cy.get('.govuk-link').contains('Change');
+    cy.get('.govuk-link').contains('Change');
 
-  //   cy.a11y();
-  // });
+    cy.a11y();
+  });
 
-  // describe('Filter results', () => {
-  //   it('Active only, with restrictions, without restrictions', () => {
-  //     cy.get('#activeEvents').check();
-  //     cy.get('#withRestrictions').check();
-  //     cy.get('#withoutRestrictions').check();
+  describe('Filter results', () => {
+    it('Active only, with restrictions, without restrictions', () => {
+      cy.get('#activeEvents').check();
+      cy.get('#withRestrictions').check();
+      cy.get('#withoutRestrictions').check();
 
-  //     cy.get('app-data-table').contains('Event map 1');
-  //     cy.get('app-data-table').contains('Mapping entry 3');
-  //     cy.get('app-data-table').contains('Fourth event mapping');
-  //     cy.get('app-data-table').contains('Prosecution opened');
-  //   });
+      cy.get('app-data-table').contains('Event map 1');
+      cy.get('app-data-table').contains('Mapping entry 3');
+      cy.get('app-data-table').contains('Fourth event mapping');
+      cy.get('app-data-table').contains('Prosecution opened');
+    });
 
-  //   it('Active only, without restrictions', () => {
-  //     cy.get('#withRestrictions').uncheck();
-  //     cy.get('app-data-table').contains('Event map 1');
-  //     cy.get('app-data-table').contains('Fourth event mapping');
-  //   });
+    it('Active only, without restrictions', () => {
+      cy.get('#withRestrictions').uncheck();
+      cy.get('app-data-table').contains('Event map 1');
+      cy.get('app-data-table').contains('Fourth event mapping');
+    });
 
-  //   it('Active and inactive, with restrictions', () => {
-  //     cy.get('#activeAndInactiveEvents').check();
-  //     cy.get('#withRestrictions').check();
-  //     cy.get('#withoutRestrictions').check();
+    it('Active and inactive, with restrictions', () => {
+      cy.get('#activeAndInactiveEvents').check();
+      cy.get('#withRestrictions').check();
+      cy.get('#withoutRestrictions').check();
 
-  //     cy.get('app-data-table').contains('Event map 2');
-  //     cy.get('app-data-table').contains('Mapping entry 3');
-  //     cy.get('app-data-table').contains('Prosecution opened');
-  //   });
+      cy.get('app-data-table').contains('Event map 2');
+      cy.get('app-data-table').contains('Mapping entry 3');
+      cy.get('app-data-table').contains('Prosecution opened');
+    });
 
-  //   it('Active and inactive, with restrictions, without restrictions, event handler set to StandardEventHandler', () => {
-  //     cy.get('#eventSearch').select('StandardEventHandler');
+    it('Active and inactive, with restrictions, without restrictions, event handler set to StandardEventHandler', () => {
+      cy.get('#eventSearch').select('StandardEventHandler');
 
-  //     cy.get('app-data-table').contains('Event map 1');
-  //     cy.get('app-data-table').contains('Fourth event mapping');
-  //     cy.get('app-data-table').contains('Prosecution opened');
-  //   });
+      cy.get('app-data-table').contains('Event map 1');
+      cy.get('app-data-table').contains('Fourth event mapping');
+      cy.get('app-data-table').contains('Prosecution opened');
+    });
 
-  //   it('Filter via search text, no results', () => {
-  //     cy.get('#eventSearch').select('StandardEventHandler');
+    it('Filter via search text, no results', () => {
+      cy.get('#eventSearch').select('StandardEventHandler');
 
-  //     cy.get('#search').type('Mapping entry 3');
+      cy.get('#search').type('Mapping entry 3');
 
-  //     cy.get('#no-data-message').contains('There are no matching results.');
-  //   });
+      cy.get('#no-data-message').contains('There are no matching results.');
+    });
 
-  //   it('Filter via search text', () => {
-  //     cy.get('#search').type('Prosecution opened');
+    it('Filter via search text', () => {
+      cy.get('#search').type('Prosecution opened');
 
-  //     cy.get('app-data-table').contains('01 Feb 2024');
-  //     cy.get('app-data-table').contains('Prosecution opened');
-  //     cy.get('app-data-table').contains('StandardEventHandler');
-  //   });
-  // });
+      cy.get('app-data-table').contains('01 Feb 2024');
+      cy.get('app-data-table').contains('Prosecution opened');
+      cy.get('app-data-table').contains('StandardEventHandler');
+    });
+  });
 
-  // describe('Add event mapping', () => {
-  //   beforeEach(() => {
-  //     cy.contains('Add event mapping').click();
-  //   });
+  describe('Add event mapping', () => {
+    beforeEach(() => {
+      cy.contains('Add event mapping').click();
+    });
 
-  //   it('should display validation errors for required fields', () => {
-  //     cy.get('#confirmButton').click();
+    it('should display validation errors for required fields', () => {
+      cy.get('#confirmButton').click();
 
-  //     cy.get('#type')
-  //       .parent()
-  //       .within(() => {
-  //         cy.get('.govuk-error-message').should('contain', 'Enter the event type');
-  //       });
+      cy.get('#type')
+        .parent()
+        .within(() => {
+          cy.get('.govuk-error-message').should('contain', 'Enter the event type');
+        });
 
-  //     cy.get('#eventName')
-  //       .parent()
-  //       .within(() => {
-  //         cy.get('.govuk-error-message').should('contain', 'Enter the event name');
-  //       });
+      cy.get('#eventName')
+        .parent()
+        .within(() => {
+          cy.get('.govuk-error-message').should('contain', 'Enter the event name');
+        });
 
-  //     cy.get('#eventHandlerSelect')
-  //       .parent()
-  //       .within(() => {
-  //         cy.get('.govuk-error-message').should('contain', 'Select an event handler to map to');
-  //       });
+      cy.get('#eventHandlerSelect')
+        .parent()
+        .within(() => {
+          cy.get('.govuk-error-message').should('contain', 'Select an event handler to map to');
+        });
 
-  //     cy.a11y();
-  //   });
+      cy.a11y();
+    });
 
-  //   it('should submit the form with valid data', () => {
-  //     cy.get('#type').type('Test Type');
-  //     cy.get('#subType').type('Test SubType');
-  //     cy.get('#eventName').type('Test Event Name');
-  //     cy.get('#eventHandlerSelect').select('StandardEventHandler');
-  //     cy.get('#withRestrictions').check();
+    it('should submit the form with valid data', () => {
+      cy.get('#type').type('Test Type');
+      cy.get('#subType').type('Test SubType');
+      cy.get('#eventName').type('Test Event Name');
+      cy.get('#eventHandlerSelect').select('StandardEventHandler');
+      cy.get('#withRestrictions').check();
 
-  //     cy.get('#confirmButton').click();
+      cy.get('#confirmButton').click();
 
-  //     cy.contains('Event mapping added').should('be.visible');
-  //   });
+      cy.contains('Event mapping added').should('be.visible');
+    });
 
-  //   it('should not submit the form with invalid data', () => {
-  //     cy.get('#type').type('Invalid Type');
-  //     cy.get('#eventName').clear();
+    it('should not submit the form with invalid data', () => {
+      cy.get('#type').type('Invalid Type');
+      cy.get('#eventName').clear();
 
-  //     cy.get('#confirmButton').click();
+      cy.get('#confirmButton').click();
 
-  //     cy.contains('Enter the event name').should('be.visible');
-  //     cy.contains('Select an event handler to map to').should('be.visible');
-  //   });
+      cy.contains('Enter the event name').should('be.visible');
+      cy.contains('Select an event handler to map to').should('be.visible');
+    });
 
-  //   it('should not submit without unique combination of type and subtype', () => {
-  //     cy.get('#type').type('1000');
-  //     cy.get('#subType').type('1001');
-  //     cy.get('#eventName').type('Test Event Name');
-  //     cy.get('#eventHandlerSelect').select('StandardEventHandler');
-  //     cy.get('#withRestrictions').check();
+    it('should not submit without unique combination of type and subtype', () => {
+      cy.get('#type').type('1000');
+      cy.get('#subType').type('1001');
+      cy.get('#eventName').type('Test Event Name');
+      cy.get('#eventHandlerSelect').select('StandardEventHandler');
+      cy.get('#withRestrictions').check();
 
-  //     cy.get('#confirmButton').click();
+      cy.get('#confirmButton').click();
 
-  //     cy.contains('The combination of event type and subtype should be unique').should('be.visible');
+      cy.contains('The combination of event type and subtype should be unique').should('be.visible');
 
-  //     cy.get('#subType').clear();
-  //     cy.get('#subType').type('1002');
+      cy.get('#subType').clear();
+      cy.get('#subType').type('1002');
 
-  //     cy.get('#confirmButton').click();
+      cy.get('#confirmButton').click();
 
-  //     cy.contains('Event mapping added').should('be.visible');
-  //   });
+      cy.contains('Event mapping added').should('be.visible');
+    });
 
-  //   it('should cancel the form and navigate back', () => {
-  //     cy.contains('Cancel').click();
-  //     cy.contains('System configuration').should('exist');
-  //   });
-  // });
+    it('should cancel the form and navigate back', () => {
+      cy.contains('Cancel').click();
+      cy.contains('System configuration').should('exist');
+    });
+  });
 
   describe('Change event mapping', () => {
     beforeEach(() => {
