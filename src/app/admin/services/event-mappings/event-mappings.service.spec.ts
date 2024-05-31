5215a7a1b5bcf02515d25d51f45fac83526fd58a
David Mann <david.mann1@hmcts.net>
2024-05-31 15:45:15 +0100

    unit tests for event map svc

--- src/app/admin/services/event-mappings/event-mappings.service.spec.ts
+++ src/app/admin/services/event-mappings/event-mappings.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
+import { EventMappingFormValues } from '@admin-types/event-mappings/event-mapping-form-values.interface';
 import { EventMappingData } from '@admin-types/event-mappings/event-mapping.interface';
 import { EventMapping } from '@admin-types/event-mappings/event-mapping.type';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
@@ -76,5 +77,112 @@ describe('EventMappingsService', () => {
 
     expect(eventHandlers!.length).toBe(2);
     expect(eventHandlers!).toEqual(mockHandlers);
+  });
+
+  it('should create event mapping', () => {
+    const mockEventMappingForm: EventMappingFormValues = {
+      type: '1000',
+      subType: '1001',
+      eventName: 'Prosecution opened',
+      eventHandler: 'StandardEventHandler',
+      withRestrictions: false,
+    };
+
+    const mockResponse: EventMappingData = {
+      id: 1,
+      type: '1000',
+      sub_type: '1001',
+      name: 'Prosecution opened',
+      handler: 'StandardEventHandler',
+      is_active: true,
+      has_restrictions: false,
+      created_at: '2024-05-05T11:00:00Z',
+    };
+
+    service.createEventMapping(mockEventMappingForm).subscribe((response) => {
+      expect(response).toEqual(mockResponse);
+    });
+
+    const req = httpMock.expectOne('api/admin/event-mappings?is_revision=false');
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual({
+      type: '1000',
+      sub_type: '1001',
+      name: 'Prosecution opened',
+      handler: 'StandardEventHandler',
+      has_restrictions: false,
+    });
+    req.flush(mockResponse);
+  });
+
+  it('should create event mapping with revision', () => {
+    const mockEventMappingForm: EventMappingFormValues = {
+      type: '1000',
+      subType: '1001',
+      eventName: 'Prosecution opened',
+      eventHandler: 'StandardEventHandler',
+      withRestrictions: false,
+    };
+
+    const mockResponse: EventMappingData = {
+      id: 1,
+      type: '1000',
+      sub_type: '1001',
+      name: 'Prosecution opened',
+      handler: 'StandardEventHandler',
+      is_active: true,
+      has_restrictions: false,
+      created_at: '2024-05-05T11:00:00Z',
+    };
+
+    service.createEventMapping(mockEventMappingForm, true).subscribe((response) => {
+      expect(response).toEqual(mockResponse);
+    });
+
+    const req = httpMock.expectOne('api/admin/event-mappings?is_revision=true');
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual({
+      type: '1000',
+      sub_type: '1001',
+      name: 'Prosecution opened',
+      handler: 'StandardEventHandler',
+      has_restrictions: false,
+    });
+    req.flush(mockResponse);
+  });
+
+  it('should retrieve a single event mapping', () => {
+    const mockResponse: EventMappingData = {
+      id: 1,
+      type: '1000',
+      sub_type: '1001',
+      name: 'Prosecution opened',
+      handler: 'StandardEventHandler',
+      is_active: true,
+      has_restrictions: false,
+      created_at: '2024-05-05T11:00:00Z',
+    };
+
+    const expectedMapping: EventMapping = {
+      id: 1,
+      type: '1000',
+      subType: '1001',
+      name: 'Prosecution opened',
+      handler: 'StandardEventHandler',
+      isActive: true,
+      hasRestrictions: false,
+      createdAt: DateTime.fromISO('2024-05-05T11:00:00Z'),
+    };
+
+    let eventMapping: EventMapping | undefined;
+    service.getEventMapping(1).subscribe((mapping) => {
+      eventMapping = mapping;
+    });
+
+    const req = httpMock.expectOne('api/admin/event-mappings/1');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockResponse);
+
+    expect(eventMapping).toEqual(expectedMapping);
   });
 });
