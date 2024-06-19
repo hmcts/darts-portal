b8eb6750225a04120f14705d655b3fc977ce9b79
David Mann <david.mann1@hmcts.net>
2024-06-19 13:57:11 +0100

    admin/user portal links, fix deprecated RouterTestingModule, unit tests

--- src/app/core/components/layout/header/header.component.spec.ts
+++ src/app/core/components/layout/header/header.component.spec.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { Router } from '@angular/router';
-import { RouterTestingModule } from '@angular/router/testing';
+import { Router, provideRouter } from '@angular/router';
 import { AppInsightsService } from '@services/app-insights/app-insights.service';
 import { AuthService } from '@services/auth/auth.service';
 import { CountNotificationService } from '@services/count-notification/count-notification.service';
@@ -37,8 +36,9 @@ describe('HeaderComponent', () => {
     };
 
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule.withRoutes([{ path: 'admin', component: DummyComponent }]), HeaderComponent],
+      imports: [HeaderComponent],
       providers: [
+        provideRouter([{ path: 'admin', component: DummyComponent }]),
         { provide: AppInsightsService, useValue: fakeAppInsightsService },
         { provide: AuthService, useValue: fakeAuthService },
         { provide: CountNotificationService, useValue: fakeCountService },
@@ -90,6 +90,57 @@ describe('HeaderComponent', () => {
     it('show admin navigation', () => {
       const adminNav = fixture.debugElement.query(By.css('#admin-nav'));
       expect(adminNav).toBeTruthy();
+    });
+  });
+
+  describe('User portal links', () => {
+    beforeEach(async () => {
+      fakeAuthService.getAuthenticated = jest.fn(() => true);
+      fakeUserService.isAdmin = jest.fn(() => true);
+      await router.navigate(['/admin']);
+      fixture.detectChanges();
+    });
+
+    it('should show user portal links', () => {
+      const userLink = fixture.debugElement.query(By.css('#user-portal-link'));
+      expect(userLink).toBeTruthy();
+    });
+
+    it('should not show admin portal links whilst on admin portal', () => {
+      const adminLink = fixture.debugElement.query(By.css('#admin-portal-link'));
+      expect(adminLink).toBeFalsy();
+    });
+  });
+
+  describe('Admin portal links', () => {
+    it('should not show admin portal link', async () => {
+      fakeAuthService.getAuthenticated = jest.fn(() => true);
+      fakeUserService.isAdmin = jest.fn(() => false);
+      await router.navigate(['/']);
+      fixture.detectChanges();
+
+      const adminLink = fixture.debugElement.query(By.css('#admin-portal-link'));
+      expect(adminLink).toBeFalsy();
+    });
+
+    it('should show admin portal link', async () => {
+      fakeAuthService.getAuthenticated = jest.fn(() => true);
+      fakeUserService.isAdmin = jest.fn(() => true);
+      await router.navigate(['/']);
+      fixture.detectChanges();
+
+      const adminLink = fixture.debugElement.query(By.css('#admin-portal-link'));
+      expect(adminLink).toBeTruthy();
+    });
+
+    it('should not show user portal link whilst on user portal', async () => {
+      fakeAuthService.getAuthenticated = jest.fn(() => true);
+      fakeUserService.isAdmin = jest.fn(() => true);
+      await router.navigate(['/']);
+      fixture.detectChanges();
+
+      const userLink = fixture.debugElement.query(By.css('#user-portal-link'));
+      expect(userLink).toBeFalsy();
     });
   });
 
