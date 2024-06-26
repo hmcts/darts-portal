5c17e6765678453d3e40976d4f0997f5a1c8a9e2
Dean Bullock <125365979+deanbullock-hmcts@users.noreply.github.com>
2024-06-26 15:00:20 +0100

    DMP-3390: Update JUDGE role name to JUDICIARY (#1213)

--- server/controllers/download.ts
+++ server/controllers/download.ts
@@ -1,12 +1,12 @@
 import * as express from 'express';
-import { Router, Request, Response } from 'express';
+import { Request, Response, Router } from 'express';
 import path from 'path';
 
 export function init(): Router {
   const router = express.Router();
 
   router.get('/annotations/template', (req: Request, res: Response) => {
-    const oneOfRequiredRoles: string[] = ['JUDGE', 'SUPER_ADMIN'];
+    const oneOfRequiredRoles: string[] = ['JUDICIARY', 'SUPER_ADMIN'];
     const userRoles = req?.session?.securityToken?.userState?.roles?.map((role) => role.roleName);
 
     if (!userRoles?.some((role) => oneOfRequiredRoles.includes(role))) {
