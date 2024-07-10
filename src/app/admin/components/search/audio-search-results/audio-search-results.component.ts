51e0e9a0ee7ad7cabf4dd21da8a796880c570269
Dean Bullock <dean.bullock@hmcts.net>
2024-07-10 15:12:11 +0100

    Remove 'Hearing Date' column from audio search results

--- src/app/admin/components/search/audio-search-results/audio-search-results.component.ts
+++ src/app/admin/components/search/audio-search-results/audio-search-results.component.ts
@@ -1,10 +1,10 @@
+import { AdminMediaSearchResult } from '@admin-types/search/admin-media-search-result';
 import { Component, computed, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { DataTableComponent } from '@common/data-table/data-table.component';
 import { DatatableColumn } from '@core-types/index';
 import { TableRowTemplateDirective } from '@directives/table-row-template.directive';
 import { LuxonDatePipe } from '@pipes/luxon-date.pipe';
-import { AdminMediaSearchResult } from './../../../models/search/admin-media-search-result';
 
 @Component({
   selector: 'app-audio-search-results',
@@ -21,7 +21,6 @@ export class AudioSearchResultsComponent {
     { name: 'Audio ID', prop: 'id', sortable: true },
     { name: 'Courthouse', prop: 'courthouse', sortable: true },
     { name: 'Courtroom', prop: 'courtroom', sortable: true },
-    { name: 'Hearing Date', prop: 'hearingDate', sortable: true },
     { name: 'Start Time', prop: 'startAt', sortable: true },
     { name: 'End Time', prop: 'endAt', sortable: true },
     { name: 'Channel', prop: 'channel', sortable: true },
