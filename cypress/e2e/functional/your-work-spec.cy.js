382271e6e021e41b04367a501d074692b01ff95c
David Mann <david.mann1@hmcts.net>
2024-07-12 09:20:07 +0100

    update count in your-work-spec ft

--- cypress/e2e/functional/your-work-spec.cy.js
+++ cypress/e2e/functional/your-work-spec.cy.js
@@ -7,10 +7,9 @@ describe('Your work', () => {
     cy.injectAxe();
   });
 
-  //TO DO: Tests should pass in isolation, count is 3 in isolation. 6 when run as suite.
   it('shows "Your work"', () => {
     cy.contains('Your work').click();
-    cy.get('#todo-tab .count').should('contain', '6');
+    cy.get('#todo-tab .count').should('contain', '3');
     cy.get('h1').should('contain', 'Your work');
     cy.a11y();
   });
