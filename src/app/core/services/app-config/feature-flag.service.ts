fa1ab6c2a92b9b9d1832c2f4c7e3ce677d2c5fcf
Sabah u Din Irfan <sabah.irfan@gmail.com>
2024-10-08 10:59:09 +0100

    remove constructor injection

--- src/app/core/services/app-config/feature-flag.service.ts
+++ src/app/core/services/app-config/feature-flag.service.ts
@@ -1,9 +1,9 @@
-import { Inject, Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { AppConfigService } from '@services/app-config/app-config.service';
 
 @Injectable({ providedIn: 'root' })
 export class FeatureFlagService {
-  constructor(@Inject(AppConfigService) private appConfigService: AppConfigService) {}
+  private appConfigService = inject(AppConfigService);
 
   isManualDeletionEnabled(): boolean {
     const enabled = this.appConfigService.getAppConfig()?.features?.manualDeletion?.enabled;
