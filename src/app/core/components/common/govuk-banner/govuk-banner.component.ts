37950059a7a0c854c201d74a686d63580b7a9120
Dean Bullock <125365979+dean-bullock@users.noreply.github.com>
2024-11-20 15:14:30 +0000

    DMP-4280: Add configurable aria label to govuk banner component (#1668)

--- src/app/core/components/common/govuk-banner/govuk-banner.component.ts
+++ src/app/core/components/common/govuk-banner/govuk-banner.component.ts
@@ -1,15 +1,15 @@
 import { Component, Input } from '@angular/core';
-import { GovukHeadingComponent } from '@common/govuk-heading/govuk-heading.component';
 
 export type GovukBannerType = 'success' | 'warning' | 'information';
 @Component({
   selector: 'app-govuk-banner',
   standalone: true,
-  imports: [GovukHeadingComponent],
+  imports: [],
   templateUrl: './govuk-banner.component.html',
   styleUrls: ['./govuk-banner.component.scss'],
 })
 export class GovukBannerComponent {
   @Input() text!: string;
   @Input() type: GovukBannerType = 'success';
+  @Input() ariaLabel!: string;
 }
