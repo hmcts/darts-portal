7ff5f2e3b4f498e4ddace61be77a37c56f5aa4c5
Dave Thompson <davethompson21@gmail.com>
2024-12-03 16:08:18 +0000

    Tidying up and moving Urls out to separate file

--- server/middleware/is-authenticated.ts
+++ server/middleware/is-authenticated.ts
@@ -1,45 +1,27 @@
-import { NextFunction, Request, Response } from 'express';
-import config from 'config';
+import { Request } from 'express';
 import { DateTime } from 'luxon';
-import { AuthenticationUtils } from '../utils/authentication-utils';
+import { AuthenticationUtils, Urls } from '../utils';
 
-const EXTERNAL_REFRESH_ACCESS_TOKEN = `${config.get('darts-api.url')}/external-user/refresh-access-token`;
-const INTERNAL_REFRESH_ACCESS_TOKEN = `${config.get('darts-api.url')}/internal-user/refresh-access-token`;
-
-export default async (req: Request, res: Response, next: NextFunction) => {
+export default async (req: Request) => {
   const expiry = req.session?.expiry;
   const sessionExpired = expiry && DateTime.now() > DateTime.fromISO(expiry);
   const userIdNotPresent = !req.session.securityToken?.userState?.userId;
   const refreshToken = req.session.securityToken?.refreshToken;
 
-  console.log('is-authenticated::accessToken', req.session?.securityToken?.accessToken);
   if (sessionExpired || userIdNotPresent || !refreshToken) {
-    console.log(
-      'is-authenticated::FALSE sessionExpired=',
-      sessionExpired,
-      'userIdNotPresent=',
-      userIdNotPresent,
-      'refreshTokenPresent=',
-      Boolean(refreshToken)
-    );
-    res.sendStatus(401);
-    return;
+    throw new Error('Session expired, userId not found, or refresh token not found');
   }
-  if (AuthenticationUtils.isJwtExpired(req.session?.securityToken?.accessToken) || Math.random() <= 0.06) {
-    const userType = req.session.userType;
-    const url = userType === 'internal' ? INTERNAL_REFRESH_ACCESS_TOKEN : EXTERNAL_REFRESH_ACCESS_TOKEN;
-
-    console.log('is-authenticated::JWT expired, using refresh token');
+  if (AuthenticationUtils.isJwtExpired(req.session?.securityToken?.accessToken)) {
     try {
-      const securityToken = await AuthenticationUtils.refreshJwt(url, refreshToken);
-      console.log('is-authenticated::JWT expired, new securityToken', securityToken);
+      const securityToken = await AuthenticationUtils.refreshJwt(
+        Urls.getRefreshAccessTokenUrl(req.session.userType!),
+        refreshToken
+      );
       req.session.securityToken = securityToken;
-      next();
+      console.log('Refreshed access token using refresh token');
     } catch (err) {
       console.log('Error refreshing access token using refresh token', err);
-      res.sendStatus(401);
+      throw err;
     }
-  } else {
-    next();
   }
 };
