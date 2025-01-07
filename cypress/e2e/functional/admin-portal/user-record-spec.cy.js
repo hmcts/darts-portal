9754bb9780cfec3290e53b72d7f745664f24347a
David Mann <david.mann1@hmcts.net>
2025-01-07 10:59:00 +0000

    user record ft fix

--- cypress/e2e/functional/admin-portal/user-record-spec.cy.js
+++ cypress/e2e/functional/admin-portal/user-record-spec.cy.js
@@ -365,7 +365,7 @@ describe('Admin - User record screen', () => {
           cy.get('td').eq(1).should('contain.text', 'C0000000007');
           cy.get('td').eq(2).should('contain.text', 'Southampton');
           cy.get('td').eq(3).should('contain.text', '06 Jan 2022');
-          cy.get('td').eq(4).should('contain.text', '06 Aug 2024 17:54');
+          cy.get('td').eq(4).should('contain.text', '07 Aug 2024 11:33');
           cy.get('td').eq(5).should('contain.text', '06 Jan 2023 13:00');
           cy.get('td').eq(6).should('contain.text', 'With Transcriber');
           cy.get('td').eq(7).should('contain.text', 'Manual');
