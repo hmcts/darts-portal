20e1c32a7b5754b9caf9b8b109b41c8e1e617cff
DavidTMann <d.mann@cgi.com>
2025-01-08 10:26:53 +0000

    DMP-4369 Validation updates across admin forms with cy tests (#1744)

--- src/app/admin/components/courthouses/create-courthouse/create-update-courthouse-form/create-update-courthouse-form.component.ts
+++ src/app/admin/components/courthouses/create-courthouse/create-update-courthouse-form/create-update-courthouse-form.component.ts
@@ -11,11 +11,13 @@ import {
   displayNameExistsValidator,
   valueIsNull,
 } from '@validators/courthouse.validator';
+import { optionalMaxLengthValidator } from '@validators/optional-maxlength.validator';
 
 const controlErrors: FormErrorMessages = {
   courthouseName: {
-    required: 'Enter a courthouse code',
-    courthouseNameExists: 'The courthouse code you entered exists already',
+    required: 'Enter a courthouse name',
+    courthouseNameExists: 'The courthouse name you entered exists already',
+    maxlength: 'Courtroom name must be less than or equal to 64 characters',
   },
   displayName: {
     required: 'Enter a display name',
@@ -69,7 +71,7 @@ export class CreateUpdateCourthouseFormComponent implements OnInit {
     this.form = this.fb.group({
       courthouseName: [
         this.formDefaultValues.courthouseName,
-        [Validators.required, courthouseNameExistsValidator(this.courthouses)],
+        [Validators.required, courthouseNameExistsValidator(this.courthouses), optionalMaxLengthValidator(64)],
       ],
       displayName: [
         this.formDefaultValues.displayName,
--- src/app/admin/constants/admin-search-form-error-messages.ts
+++ src/app/admin/constants/admin-search-form-error-messages.ts
@@ -1,4 +1,7 @@
 export const AdminSearchFormErrorMessages = {
+  courtroom: {
+    maxlength: 'Courtroom name must be less than or equal to 64 characters',
+  },
   specific: {
     pattern: 'You have not entered a recognised date in the correct format (for example 31/01/2023)',
     futureDate: 'You have selected a date in the future. Requested date must be in the past',
