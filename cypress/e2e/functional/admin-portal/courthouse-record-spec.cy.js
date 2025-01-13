4b56b40741cade06cb7b3fbc10322fd1f94e4fe6
DavidTMann <d.mann@cgi.com>
2025-01-13 16:09:17 +0000

    DMP-4575 Bug fix validation limit courthouse create-edit (#1761)

--- cypress/e2e/functional/admin-portal/courthouse-record-spec.cy.js
+++ cypress/e2e/functional/admin-portal/courthouse-record-spec.cy.js
@@ -142,7 +142,8 @@ describe('Admin - Courthouse record screen', () => {
   });
 
   it('Create courthouse & validation', () => {
-    const invalidCourthouseName = ';hK+aySS}Q+b4@qrMczv9n.Kt0cHxNGr=#ZD%_&ugBg6h_qgy[vQ)TzH6@nZ?W45#'; //65 characters
+    const invalidCourthouseName =
+      '}Z]&349Fpyd#R17KneNpgNwn$&qZSAv6F[L2Qa(YR#XKpu,9zmcN[&P17]Zw+e[HJy4e%FN(wUDLSgKf7dp7b3uNZ%e9[={1V;Ma=e#V#AYREr(4)yNXVkj+J1bTjY)JB(L/VCS_{z+/+7Yt54kbqB=C0-j!WZ-j$JgM0!M2GDLk?XXdVt4cfYbFuVt1uh:R9]@c-8fJ$Np1JVww[&]fGcTvc4J1;W8[j-):z-WkH?6E,?n8*xyj9Xc]G]XS1Q_7'; //256 characters
     const courthouseName = 'COURTHOUSE';
     const displayName = 'Courthouse';
     cy.contains('button.govuk-button', 'Create new courthouse').click();
@@ -202,8 +203,8 @@ describe('Admin - Courthouse record screen', () => {
     cy.contains('.govuk-button', 'Continue').click();
 
     //Verify max length errors
-    cy.get('.govuk-error-summary').should('contain', 'Courtroom name must be less than or equal to 64 characters');
-    cy.get('.courthouse-name-error').should('contain', 'Courtroom name must be less than or equal to 64 characters');
+    cy.get('.govuk-error-summary').should('contain', 'Courtroom name must be less than or equal to 255 characters');
+    cy.get('.courthouse-name-error').should('contain', 'Courtroom name must be less than or equal to 255 characters');
 
     cy.get('#courthouse-name').clear().type(courthouseName);
 
