a5b558062da424afa97a9ccfe67e2e9bc773771f
Dave Thompson <davethompson21@gmail.com>
2025-02-10 17:03:16 +0000

    Adding app insights for node server

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import { startServer } from './server';
 
 import * as propertiesVolume from '@hmcts/properties-volume';
 import config from 'config';
+import AppInsights from './lib/app-insights';
 propertiesVolume.addTo(config);
 
 const PORT = config.get('port');
@@ -10,5 +11,6 @@ const READY_MESSAGE = `> Ready on http://localhost:${PORT}`;
 const server = startServer();
 
 server.listen(PORT, () => {
+  AppInsights.enable();
   console.log(READY_MESSAGE);
 });
