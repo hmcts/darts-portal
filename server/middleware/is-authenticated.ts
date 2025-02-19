8e76ffacea777c1dcf0bba68b230960772c3e296
Dave Thompson <davethompson21@gmail.com>
2025-02-19 11:04:07 +0000

    Allowing keep alive test requests without authentication

--- server/middleware/is-authenticated.ts
+++ server/middleware/is-authenticated.ts
@@ -3,6 +3,12 @@ import { AuthenticationUtils } from '../utils/authentication-utils';
 import { DateTime } from 'luxon';
 
 export default (req: Request, res: Response, next: NextFunction): void => {
+  // temporary code for testing keep-alive request, to be removed
+  if (req.url === '/keep-alive-test' || req.url === '/keep-alive-test-stream') {
+    return next();
+  }
+  // temporary code for testing keep-alive request, to be removed
+
   const expiry = req.session?.expiry;
   const sessionExpired = expiry && DateTime.now() > DateTime.fromISO(expiry);
   if (
