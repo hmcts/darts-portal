5d5b95a16e77576badac4d55b0c6b27177b72a90
David Mann <david.mann1@hmcts.net>
2025-03-04 10:30:46 +0000

    report restrictions, expired banner conditions on admin case file

--- src/app/admin/components/case/case-file/case-file.component.ts
+++ src/app/admin/components/case/case-file/case-file.component.ts
@@ -5,10 +5,20 @@ import { GovukHeadingComponent } from '@common/govuk-heading/govuk-heading.compo
 import { GovukSummaryListDirectives } from '@directives/govuk-summary-list';
 import { DateTimePipe } from '@pipes/dateTime.pipe';
 import { JoinPipe } from '@pipes/join';
+import { ExpiredBannerComponent } from '../../../../core/components/common/expired-banner/expired-banner.component';
+import { ReportingRestrictionComponent } from '../../../../core/components/common/reporting-restriction/reporting-restriction.component';
 
 @Component({
   selector: 'app-case-file',
-  imports: [GovukHeadingComponent, JoinPipe, DateTimePipe, RouterLink, GovukSummaryListDirectives],
+  imports: [
+    GovukHeadingComponent,
+    JoinPipe,
+    DateTimePipe,
+    RouterLink,
+    GovukSummaryListDirectives,
+    ReportingRestrictionComponent,
+    ExpiredBannerComponent,
+  ],
   templateUrl: './case-file.component.html',
   styleUrl: './case-file.component.scss',
 })
--- src/app/admin/components/case/case.component.ts
+++ src/app/admin/components/case/case.component.ts
@@ -1,22 +1,25 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, inject, input, numberAttribute } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { LoadingComponent } from '@common/loading/loading.component';
-import { TabsComponent } from '@common/tabs/tabs.component';
-import { TabDirective } from '@directives/tab.directive';
 import { AdminCaseService } from '@services/admin-case/admin-case.service';
+import { HistoryService } from '@services/history/history.service';
 import { CaseFileComponent } from './case-file/case-file.component';
 
 @Component({
   selector: 'app-case',
-  imports: [CaseFileComponent, RouterLink, LoadingComponent, AsyncPipe, TabsComponent, TabDirective],
+  imports: [CaseFileComponent, RouterLink, LoadingComponent, AsyncPipe],
   templateUrl: './case.component.html',
   styleUrl: './case.component.scss',
 })
 export class CaseComponent {
   caseService = inject(AdminCaseService);
+  historyService = inject(HistoryService);
+  url = inject(Router).url;
 
   caseId = input(0, { transform: numberAttribute });
 
   caseFile$ = this.caseService.getCase(this.caseId());
+
+  backUrl = this.historyService.getBackUrl(this.url) ?? '/admin/search';
 }
