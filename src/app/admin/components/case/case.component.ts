720abe05fdf949d741fa445b55d006892d5d1982
David Mann <david.mann1@hmcts.net>
2025-03-04 12:41:57 +0000

    case hearing table admin

--- src/app/admin/components/case/case.component.ts
+++ src/app/admin/components/case/case.component.ts
@@ -1,25 +1,47 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, inject, input, numberAttribute } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { GovukHeadingComponent } from '@common/govuk-heading/govuk-heading.component';
 import { LoadingComponent } from '@common/loading/loading.component';
+import { TabsComponent } from '@common/tabs/tabs.component';
+import { CaseHearingsTableComponent } from '@components/case/case-file/case-hearings-table/case-hearings-table.component';
+import { TabDirective } from '@directives/tab.directive';
 import { AdminCaseService } from '@services/admin-case/admin-case.service';
+import { CaseService } from '@services/case/case.service';
 import { HistoryService } from '@services/history/history.service';
+import { catchError, combineLatest, of } from 'rxjs';
 import { CaseFileComponent } from './case-file/case-file.component';
 
 @Component({
   selector: 'app-case',
-  imports: [CaseFileComponent, RouterLink, LoadingComponent, AsyncPipe],
+  imports: [
+    CaseFileComponent,
+    RouterLink,
+    LoadingComponent,
+    AsyncPipe,
+    TabsComponent,
+    TabDirective,
+    CaseHearingsTableComponent,
+    GovukHeadingComponent,
+  ],
   templateUrl: './case.component.html',
   styleUrl: './case.component.scss',
 })
 export class CaseComponent {
-  caseService = inject(AdminCaseService);
+  caseAdminService = inject(AdminCaseService);
+  caseService = inject(CaseService);
   historyService = inject(HistoryService);
   url = inject(Router).url;
 
   caseId = input(0, { transform: numberAttribute });
 
-  caseFile$ = this.caseService.getCase(this.caseId());
+  caseFile$ = this.caseAdminService.getCase(this.caseId());
+  hearings$ = this.caseService.getCaseHearings(this.caseId());
 
   backUrl = this.historyService.getBackUrl(this.url) ?? '/admin/search';
+
+  data$ = combineLatest({
+    caseFile: this.caseFile$,
+    hearings: this.hearings$.pipe(catchError(() => of(null))),
+  });
 }
