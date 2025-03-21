e50f809570b927082c6ba0da32e970f10a655e90
Dave Thompson <david.thompson@hmcts.net>
2025-03-21 14:09:19 +0000

    DMP-4827 Adding specific error message for case search 504 timeout response (#1897)

--- src/app/portal/components/search/search-error/search-error.component.ts
+++ src/app/portal/components/search/search-error/search-error.component.ts
@@ -3,6 +3,7 @@ import { AfterViewInit, Component, Input, NgZone, inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { InternalErrorComponent } from '@components/error/internal-server/internal-error.component';
 import { ErrorMessage } from '@core-types/error/error-message.interface';
+import { AppConfigService } from '@services/app-config/app-config.service';
 import { HeaderService } from '@services/header/header.service';
 
 @Component({
@@ -16,11 +17,14 @@ export class SearchErrorComponent implements AfterViewInit {
   @Input() error!: ErrorMessage | null;
 
   headerService = inject(HeaderService);
+  appConfigService = inject(AppConfigService);
   router = inject(Router);
-  private zone = inject(NgZone);
+  private readonly zone = inject(NgZone);
+
+  timeout = this.appConfigService.getAppConfig()?.caseSearchTimeout;
 
   ngAfterViewInit(): void {
-    if (this.error?.display === 'PAGE' && this.error.status === 500) {
+    if (this.error?.display === 'PAGE' && [500, 504].includes(this.error.status)) {
       this.zone.run(() => {
         this.router.navigateByUrl('internal-error');
       });
