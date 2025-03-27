842269fd8348fc5ec4fffaf4ed125013b14c689f
Dave Thompson <davethompson21@gmail.com>
2025-03-27 10:41:10 +0000

    Adding graceful shutdown

--- server/index.ts
+++ server/index.ts
@@ -3,12 +3,20 @@ import { startServer } from './server';
 import * as propertiesVolume from '@hmcts/properties-volume';
 import config from 'config';
 propertiesVolume.addTo(config);
+import * as GracefulShutdownManager from '@moebius/http-graceful-shutdown';
 
 const PORT = config.get('port');
 const READY_MESSAGE = `> Ready on http://localhost:${PORT}`;
 
-const server = startServer();
-
-server.listen(PORT, () => {
+const express = startServer();
+const server = express.listen(PORT, () => {
   console.log(READY_MESSAGE);
+});
+
+const shutdownManager = new GracefulShutdownManager.GracefulShutdownManager(server);
+
+process.on('SIGTERM', () => {
+  shutdownManager.terminate(() => {
+    console.log('Server is gracefully terminated');
+  });
 });
