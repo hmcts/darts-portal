774b4a7f6fea6e61af902701d28da34537105eda
Dave Thompson <davethompson21@gmail.com>
2025-04-08 13:21:15 +0100

    Graceful shutdown fixes

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,5 @@ RUN yarn build
 ENV NODE_ENV=production
 
 EXPOSE 3000
+
+CMD ["node", "dist/server/server"]
--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,6 @@ import { startServer } from './server';
 import * as propertiesVolume from '@hmcts/properties-volume';
 import config from 'config';
 propertiesVolume.addTo(config);
-import * as GracefulShutdownManager from '@moebius/http-graceful-shutdown';
 
 const PORT = config.get('port');
 const READY_MESSAGE = `> Ready on http://localhost:${PORT}`;
@@ -13,10 +12,19 @@ const server = express.listen(PORT, () => {
   console.log(READY_MESSAGE);
 });
 
-const shutdownManager = new GracefulShutdownManager.GracefulShutdownManager(server);
+async function stopServer() {
+  console.info('Express server shutdown signal received');
+  console.info('Express server closing down');
+  server.close(() => {
+    console.log('Express server closing down completed');
+    process.exit(0);
+  });
+}
+
+process.on('SIGINT', function () {
+  stopServer();
+});
 
 process.on('SIGTERM', () => {
-  shutdownManager.terminate(() => {
-    console.log('Server is gracefully terminated');
-  });
+  stopServer();
 });
