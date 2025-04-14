42955e42afcb414e523ebdc307a02ed3843be059
David Mann <david.mann1@hmcts.net>
2025-04-14 16:39:53 +0100

    preserving line breaks on judge, defendants

--- src/app/core/directives/govuk-summary-list/govuk-summary-value.directive.ts
+++ src/app/core/directives/govuk-summary-list/govuk-summary-value.directive.ts
@@ -1,8 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[govukSummaryValue]',
   standalone: true,
-  host: { class: 'govuk-summary-list__value' },
+  host: {
+    class: 'govuk-summary-list__value',
+  },
 })
-export class GovukSummaryValueDirective {}
+export class GovukSummaryValueDirective implements OnInit {
+  @Input() preserveLineBreaks = false;
+
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2
+  ) {}
+
+  ngOnInit(): void {
+    if (this.preserveLineBreaks) {
+      this.renderer.setStyle(this.el.nativeElement, 'white-space', 'pre-line');
+    }
+  }
+}
