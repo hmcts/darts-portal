960e8f1671c6d4e467bf432440ae9704af41ede3
David Mann <david.mann1@hmcts.net>
2025-04-22 12:48:41 +0100

    remove logs

--- src/app/admin/services/admin-search/admin-search.service.ts
+++ src/app/admin/services/admin-search/admin-search.service.ts
@@ -119,9 +119,6 @@ export class AdminSearchService {
     this.isLoading.set(false);
     this.formValues.set({ ...defaultFormValues });
     this.hasFormBeenSubmitted.set(false);
-
-    console.log('Search cleared');
-    console.log(this.formValues()); //remove me
   }
 
   private logSearchEvent(type: SearchType, formValues: SearchRequestBody) {
