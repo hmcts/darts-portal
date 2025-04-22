e7ca5a7914bf1f42fa9649433feb8528e28ea3cb
Ben Edwards <147524406+Ben-Edwards-cgi@users.noreply.github.com>
2025-04-22 09:51:58 +0100

    DMP-4936: ENHANCMENT: Remove Owner from advanced search on transcript requests and completed transcripts (#1967)

--- src/app/admin/models/transcription/transcription-search-request.interface.ts
+++ src/app/admin/models/transcription/transcription-search-request.interface.ts
@@ -3,7 +3,6 @@ export interface TranscriptionSearchRequest {
   case_number: string | null;
   courthouse_display_name: string | null;
   hearing_date: string | null;
-  owner: string | null;
   requested_by: string | null;
   requested_at_from: string | null;
   requested_at_to: string | null;
