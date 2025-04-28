a16e2d58a06608137cb3549a783a02fac39a6e25
benedwards <ben.edwards@cgi.com>
2025-04-28 17:45:48 +0100

    Added banner focus

--- src/app/core/components/common/govuk-banner/govuk-banner.component.ts
+++ src/app/core/components/common/govuk-banner/govuk-banner.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import {AfterViewInit, booleanAttribute, Component, ElementRef, Input, OnDestroy} from '@angular/core';
 
 export type GovukBannerType = 'success' | 'warning' | 'information';
+
 @Component({
   selector: 'app-govuk-banner',
   standalone: true,
@@ -8,8 +9,44 @@ export type GovukBannerType = 'success' | 'warning' | 'information';
   templateUrl: './govuk-banner.component.html',
   styleUrls: ['./govuk-banner.component.scss'],
 })
-export class GovukBannerComponent {
+export class GovukBannerComponent implements AfterViewInit, OnDestroy {
+  private observer!: MutationObserver;
   @Input() text!: string;
   @Input() type: GovukBannerType = 'success';
   @Input() ariaLabel!: string;
+  @Input({transform: booleanAttribute}) focusOnChange: boolean = false;
+
+  constructor(private elementRef: ElementRef) {
+  }
+
+  ngAfterViewInit() {
+    if (!this.focusOnChange) {
+      return;
+    }
+    const ngContent = this.elementRef.nativeElement;
+
+    this.observer = new MutationObserver((mutations) => {
+      this.focusBanner();
+    });
+
+    this.observer.observe(ngContent, {
+      childList: true,
+      subtree: true,
+      characterData: true,
+    });
+    this.focusBanner();
+  }
+
+  ngOnDestroy() {
+    if (this.observer) {
+      this.observer.disconnect();
+    }
+  }
+
+  private focusBanner() {
+    this.elementRef.nativeElement.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  }
 }
