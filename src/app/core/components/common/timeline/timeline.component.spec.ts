e055cb6dd0a562fbd624e5f76468e7cdf2b3559f
benedwards <ben.edwards@cgi.com>
2025-05-02 10:58:07 +0100

    Added tests

--- src/app/core/components/common/timeline/timeline.component.spec.ts
+++ src/app/core/components/common/timeline/timeline.component.spec.ts
@@ -1,11 +1,11 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { DatePipe } from '@angular/common';
-import { By } from '@angular/platform-browser';
-import { ActivatedRoute } from '@angular/router';
-import { TimelineItem } from '@core-types/index';
-import { DateTime } from 'luxon';
-import { TimelineComponent } from './timeline.component';
+import {DatePipe} from '@angular/common';
+import {By} from '@angular/platform-browser';
+import {ActivatedRoute} from '@angular/router';
+import {TimelineItem} from '@core-types/index';
+import {DateTime} from 'luxon';
+import {TimelineComponent} from './timeline.component';
 
 describe('TimelineComponent', () => {
   let component: TimelineComponent;
@@ -14,6 +14,7 @@ describe('TimelineComponent', () => {
   // 1 January 2021 at 12:00 AM
   const time = DateTime.fromISO('2021-01-01T00:00:00Z');
 
+  // @ts-ignore
   const MOCK_TIMELINE_ITEMS: TimelineItem[] = [
     {
       dateTime: time,
@@ -35,12 +36,19 @@ describe('TimelineComponent', () => {
         emailAddress: 'beans@toast.com',
       },
     },
+    {
+      dateTime: time,
+      title: 'Title 2',
+      descriptionLines: ['Description 2'],
+      //@ts-expect-error legacy data could be missing user
+      user: null,
+    },
   ];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [TimelineComponent],
-      providers: [{ provide: ActivatedRoute, useValue: {} }, DatePipe],
+      providers: [{provide: ActivatedRoute, useValue: {}}, DatePipe],
     }).compileComponents();
 
     fixture = TestBed.createComponent(TimelineComponent);
@@ -60,13 +68,17 @@ describe('TimelineComponent', () => {
       const itemElement = timelineItems[i];
       const titleElement = itemElement.query(By.css('.moj-timeline__title')).nativeElement;
       const descriptionElement = itemElement.query(By.css('.moj-timeline__description')).nativeElement;
-      const userElement = itemElement.query(By.css('.moj-timeline__byline')).nativeElement;
+      const userElement = itemElement.query(By.css('.moj-timeline__byline'))?.nativeElement;
       const dateTimeElement = itemElement.query(By.css('.moj-timeline__date')).nativeElement;
 
       expect(titleElement.textContent).toContain(item.title);
       expect(descriptionElement.textContent).toContain(item.descriptionLines[0]);
-      expect(userElement.textContent).toContain(item.user.fullName);
-      expect(userElement.textContent).toContain(item.user.emailAddress);
+      if (item.user !== null) {
+        expect(userElement.textContent).toContain(item.user.fullName);
+        expect(userElement.textContent).toContain(item.user.emailAddress);
+      } else {
+        expect(userElement).toBeUndefined();
+      }
       expect(dateTimeElement.textContent).toContain('1 January 2021 at 12:00 AM');
     });
   });
