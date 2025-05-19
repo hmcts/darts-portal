9b523dc7aeaf7f475e388d6f7188b0ef4130e55d
Dave Thompson <davethompson21@gmail.com>
2025-05-19 12:45:50 +0100

    Fetching version properly

--- server/app-config.ts
+++ server/app-config.ts
@@ -1,7 +1,7 @@
 import config from 'config';
+import { version } from './utils/version';
 
 // only include config items that are allowed to be exposed the browser
-
 export default () => ({
   appInsightsKey: config.get('secrets.darts.AppInsightsInstrumentationKey'),
   support: config.get('support'),
@@ -10,5 +10,5 @@ export default () => ({
   features: config.get('features'),
   pagination: config.get('pagination'),
   caseSearchTimeout: config.get('caseSearchTimeout'),
-  version: config.get('version'),
+  version: version(),
 });
