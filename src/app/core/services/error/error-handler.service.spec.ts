2a84cd04c232f6cf4f4e9d46db02ae9d36617818
Dave Thompson <david.thompson@hmcts.net>
2025-05-20 16:48:08 +0100

    DMP-5120 (Dev only) Reducing logged exceptions (#2051)

--- src/app/core/services/error/error-handler.service.spec.ts
+++ src/app/core/services/error/error-handler.service.spec.ts
@@ -1,8 +1,9 @@
 import { Injector } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { AppInsightsService } from '@services/app-insights/app-insights.service';
-import { ErrorHandlerService } from './error-handler.service';
+import { ErrorHandlerService, IGNORE_HTTP_STATUS_CODES, IMPORT_FAILED_MESSAGE } from './error-handler.service';
 import { AppConfig, AppConfigService } from '@services/app-config/app-config.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 class MockWindow {
   location = {
@@ -50,14 +51,12 @@ describe('ErrorHandlerService', () => {
 
   it('should log an exception', () => {
     const error = new Error('Test Error');
-
     errorHandlerService.handleError(error);
-
     expect(mockAppInsightsService.logException).toHaveBeenCalledWith(error);
   });
 
-  describe('"Failed to fetch dynamically imported module" errors', () => {
-    const error = new Error('Failed to fetch dynamically imported module');
+  describe('dynamic module import errors', () => {
+    const error = new Error(IMPORT_FAILED_MESSAGE.source);
 
     it('reloads page if the version has changed', async () => {
       const loadConfigPromise = Promise.resolve();
@@ -93,6 +92,60 @@ describe('ErrorHandlerService', () => {
         new Error('Import module failed but latest and cached versions are the same - 0.0.1')
       );
       expect(mockWindow.location.href).toBe('/internal-error');
+    });
+  });
+
+  describe('ignored http status codes', () => {
+    IGNORE_HTTP_STATUS_CODES.forEach((ignoredErrorCode) => {
+      it(`logs trace for error with HTTP error code ${ignoredErrorCode}`, () => {
+        const error = new HttpErrorResponse({
+          status: ignoredErrorCode,
+          statusText: 'Test error',
+          url: 'http://darts.local/test',
+        });
+        errorHandlerService.handleError(error);
+
+        expect(mockAppInsightsService.logTrace).toHaveBeenCalledWith(
+          `Http failure response for http://darts.local/test: ${ignoredErrorCode} Test error`
+        );
+      });
+    });
+  });
+
+  describe('network errors', () => {
+    it('logs trace for network errors with ProgressEvent', () => {
+      const error = new HttpErrorResponse({
+        error: new ProgressEvent('test'),
+        status: 0,
+        statusText: 'Network error',
+        url: 'http://darts.local/test',
+      });
+      errorHandlerService.handleError(error);
+
+      expect(mockAppInsightsService.logTrace).toHaveBeenCalledWith(
+        'Http failure response for http://darts.local/test: 0 Network error'
+      );
+    });
+
+    it('logs exception for network errors without ProgressEvent', () => {
+      const error = new HttpErrorResponse({
+        status: 0,
+        statusText: 'Network error',
+        url: 'http://darts.local/test',
+      });
+      errorHandlerService.handleError(error);
+
+      expect(mockAppInsightsService.logException).toHaveBeenCalledWith(
+        expect.objectContaining({
+          error: null,
+          message: 'Http failure response for http://darts.local/test: 0 Network error',
+          name: 'HttpErrorResponse',
+          ok: false,
+          status: 0,
+          statusText: 'Network error',
+          url: 'http://darts.local/test',
+        })
+      );
     });
   });
 });
--- src/app/core/services/error/error-handler.service.ts
+++ src/app/core/services/error/error-handler.service.ts
@@ -1,6 +1,10 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { ErrorHandler, Inject, Injectable, Injector } from '@angular/core';
 import { AppConfigService } from '@services/app-config/app-config.service';
 import { AppInsightsService } from '@services/app-insights/app-insights.service';
+
+export const IMPORT_FAILED_MESSAGE = /Failed to fetch dynamically imported module/;
+export const IGNORE_HTTP_STATUS_CODES = [401, 422];
 
 @Injectable({
   providedIn: 'root',
@@ -24,26 +28,46 @@ export class ErrorHandlerService extends ErrorHandler {
           this.injector.get(AppInsightsService).logTrace('Import module failed, page reloaded.');
           this.window.location.reload();
         } else {
-          this.logException(
+          this.handleException(
             new Error(`Import module failed but latest and cached versions are the same - ${latestVersion}`)
           );
           this.window.location.href = '/internal-error';
         }
       })
-      .catch(this.logException);
+      .catch(this.handleException);
   }
 
-  private logException(error: Error) {
-    console.error('Exception', error);
-    this.injector.get(AppInsightsService).logException(error);
+  private isHttpErrorResponse(error: Error | HttpErrorResponse): error is HttpErrorResponse {
+    return error instanceof HttpErrorResponse;
   }
 
-  override handleError(error: Error) {
-    const importFailedMessage = /Failed to fetch dynamically imported module/;
-    if (importFailedMessage.test(error.message)) {
+  private isIgnoredHttpStatusCode(error: Error | HttpErrorResponse): boolean {
+    return this.isHttpErrorResponse(error) && IGNORE_HTTP_STATUS_CODES.includes(error?.status);
+  }
+
+  // See https://angular.dev/guide/http/making-requests#handling-request-failure
+  private isNetworkError(error: Error | HttpErrorResponse): boolean {
+    return this.isHttpErrorResponse(error) && error?.status === 0 && error?.error instanceof ProgressEvent;
+  }
+
+  private handleException(error: Error | HttpErrorResponse) {
+    const isIgnoredErrorCode = this.isIgnoredHttpStatusCode(error);
+    const isNetworkError = this.isNetworkError(error);
+    if (isIgnoredErrorCode || isNetworkError) {
+      // log as trace, not exception
+      console.log('Exception', error.message);
+      this.injector.get(AppInsightsService).logTrace(error.message);
+    } else {
+      console.error('Exception', error);
+      this.injector.get(AppInsightsService).logException(error);
+    }
+  }
+
+  override handleError(error: Error | HttpErrorResponse) {
+    if (IMPORT_FAILED_MESSAGE.test(error.message)) {
       this.handleImportModuleFailed();
     } else {
-      this.logException(error);
+      this.handleException(error);
     }
   }
 }
