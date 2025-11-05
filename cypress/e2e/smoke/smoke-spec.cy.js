480b69e4cc766fd6f91b1a1501f97276eb1afcf5
DavidTMann <d.mann@cgi.com>
2025-11-05 16:53:24 +0000

    DMP-5274 initial dev login check changes (#2219)

--- cypress/e2e/smoke/smoke-spec.cy.js
+++ cypress/e2e/smoke/smoke-spec.cy.js
@@ -1,7 +1,18 @@
+import '../functional/commands';
+import * as adLogin from '../support/ad-login';
+
 describe('Smoke test', () => {
   it('should load portal login page', () => {
     cy.visit('/login');
     cy.get('.govuk-label').should('contain', "I'm an employee of HM Courts and Tribunals Service");
     cy.get('.govuk-label').should('contain', 'I work with the HM Courts and Tribunals Service');
   });
+
+  if (adLogin.isDevPreview()) {
+    it('should login via dev preview stub for external user', () => {
+      cy.realLogin('external');
+      cy.get('app-govuk-heading').should('contain', 'Search for a case');
+      cy.logout();
+    });
+  }
 });
