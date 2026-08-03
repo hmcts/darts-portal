362711293540cfacd69c41c7e7a22f05941fc4da
Arthur Ford <arthur.ford@hmcts.net>
2026-08-03 16:47:16 +0100

    DMP-5606 ITHC HTTP Response Headers
    Of the 6 Headers identified in DMP-5606, 3 were already present. Therefore add the 3 missing headers

--- server/middleware/set-headers.ts
+++ server/middleware/set-headers.ts
@@ -4,6 +4,9 @@ export default (req: Request, res: Response, next: NextFunction): void => {
   res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
   res.setHeader('X-Frame-Options', 'DENY');
   res.setHeader('Content-Security-Policy', "frame-ancestors 'none'");
+  res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
+  res.setHeader('Permissions-Policy', 'geolocation=(), microphone=(), camera=()');
   if (req.secure) {
     res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
   }
