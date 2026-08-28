8f417ad201d0ba67b5c1198541e3ce7ad1c4a1eb
karen-hedges <133129444+karen-hedges@users.noreply.github.com>
2026-08-28 18:13:39 +0100

    DMP-3712 Migrate towards the use of Spring 3 Problem Details dquote> dquote> Added handling for spring boot 3 problem details from darts-api

--- src/app/core/models/error/error-type.type.ts
+++ src/app/core/models/error/error-type.type.ts
@@ -1,6 +1,9 @@
 export interface ErrorType {
   duplicate_transcription_id?: number;
+  detail?: string;
+  instance?: string;
   type?: string;
   status?: number;
   title?: string;
+  [key: string]: unknown;
 }
